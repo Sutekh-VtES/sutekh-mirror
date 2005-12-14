92e24c97e19714ffd3a44622ba7dd0930686dd12
Simon Cross <hodgestar@gmail.com>
2005-12-14 11:22:23 +0000

    * CardTextFilter completed. * Filters now exported by sutekh package. * Brief filter usage example added to README.

--- sutekh/sutekh/Filters.py
+++ sutekh/sutekh/Filters.py
@@ -1,20 +1,24 @@
 from SutekhObjects import *
+from sqlobject import AND, OR, LIKE
 
 # Filter Base Class
 
 class Filter(object):
-    pass
+    def getExpression(self):
+        raise NotImplementedError
 
 # Collections of Filters
     
-class FilterBox(Filter):
+class FilterBox(Filter,list):
     pass
     
 class FilterAndBox(FilterBox):
-    pass
+    def getExpression(self):
+        return AND(*self)
     
 class FilterOrBox(FilterBox):
-    pass
+    def getExpression(self):
+        return OR(*self)
 
 # Individual Filters
 
@@ -26,3 +30,10 @@ class DisciplineFilter(Filter):
     
 class CardTypeFilter(Filter):
     pass
+
+class CardTextFilter(Filter):
+    def __init__(self,sPattern):
+        self.__sPattern = sPattern
+        
+    def getExpression(self):
+        return LIKE(AbstractCard.q.text,'%' + self.__sPattern + '%')
--- sutekh/sutekh/__init__.py
+++ sutekh/sutekh/__init__.py
@@ -1,8 +1,15 @@
-__all__ = ['AbstractCard', 'PhysicalCard', 'AbstractCardSet', 'PhysicalCardSet',
-           'RarityPair', 'Expansion', 'Rarity', 'DisciplinePair', 'Discipline',
-           'Clan', 'CardType', 'Ruling', 'start' ]
+__all__ = [ # Sutekh Objects
+            'AbstractCard', 'PhysicalCard', 'AbstractCardSet', 'PhysicalCardSet',
+            'RarityPair', 'Expansion', 'Rarity', 'DisciplinePair', 'Discipline',
+            'Clan', 'CardType', 'Ruling',
+            # Filters
+            'FilterAndBox', 'FilterOrBox', 'ClanFilter', 'DisciplineFilter',
+            'CardTypeFilter', 'CardTextFilter',
+            # Misc
+           'start' ]
 
 from SutekhObjects import *
+from Filters import *
 from SutekhCli import main
 
 def start(aArgs=['sutekh']):
