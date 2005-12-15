87ceb275717252a31a3d203648f4d52c792a9ac8
Simon Cross <hodgestar@gmail.com>
2005-12-15 13:29:20 +0000

    * Committing basic filtering implementation. * Usage example in README.

--- sutekh/sutekh/Filters.py
+++ sutekh/sutekh/Filters.py
@@ -1,5 +1,6 @@
 from SutekhObjects import *
 from sqlobject import AND, OR, LIKE
+from sqlobject.sqlbuilder import Table
 
 # Filter Base Class
 
@@ -14,22 +15,41 @@ class FilterBox(Filter,list):
     
 class FilterAndBox(FilterBox):
     def getExpression(self):
-        return AND(*self)
+        return AND(*[x.getExpression() for x in self])
     
 class FilterOrBox(FilterBox):
     def getExpression(self):
-        return OR(*self)
+        return OR(*[x.getExpression() for x in self])
 
 # Individual Filters
 
 class ClanFilter(Filter):
-    pass
+    def __init__(self,sClan):
+        self.__oClan = IClan(sClan)
+        
+    def getExpression(self):
+        oT = Table('abs_clan_map')
+        return AND(AbstractCard.q.id == oT.abstract_card_id,
+                   oT.clan_id == self.__oClan.id)
     
 class DisciplineFilter(Filter):
-    pass
+    def __init__(self,sDiscipline):
+        self.__oDis = IDiscipline(sDiscipline)
+        
+    def getExpression(self):
+        oT = Table('abs_discipline_pair_map')
+        return AND(AbstractCard.q.id == oT.abstract_card_id,
+                   oT.discipline_pair_id == DisciplinePair.q.id,
+                   DisciplinePair.q.disciplineID == self.__oDis.id)
     
 class CardTypeFilter(Filter):
-    pass
+    def __init__(self,sCardType):
+        self.__oType = ICardType(sCardType)
+        
+    def getExpression(self):
+        oT = Table('abs_type_map')
+        return AND(AbstractCard.q.id == oT.abstract_card_id,
+                   oT.card_type_id == self.__oType.id)
 
 class CardTextFilter(Filter):
     def __init__(self,sPattern):
