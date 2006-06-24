17b4bc4c054873455404eafdd8ce6a61949e8508
Simon Cross <hodgestar@gmail.com>
2006-06-24 14:55:00 +0000

    Skeleton for plugins package.

---
+++ sutekh/sutekh/gui/plugins/ClusterCardList.py
@@ -0,0 +1,3 @@
+# ClusterCardList.py
+# Copyright 2006 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
---
+++ sutekh/sutekh/gui/plugins/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+# Copyright 2006 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
+
