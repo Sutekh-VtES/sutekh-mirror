055c66e419581b53aa4a35e51904bdd5506b5f4c
Simon Cross <hodgestar@gmail.com>
2006-06-24 01:44:13 +0000

    Class for creating tables for card list clustering, and possibly other purposes.

---
+++ sutekh/sutekh/CardListTabulator.py
@@ -0,0 +1,88 @@
+# CardListTabulator.py
+# Copyright 2006 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
+
+from SutekhObjects import *
+
+class CardListTabulator(object):
+    """
+    Creates a table of cards from a card list.
+    Each row of the table corresponds to a card in the list.   
+    Each column of the table represents a numerical property of the card (pool cost, blood cost, discipline levels etc).
+    """
+
+    def __init__(self,aColNames,dPropFuncs):
+        """
+        aColNames: Keys from dPropFuncs in the order in which you want the table columns to appear.
+        dPropFuncs: Dictionary of functions for finding properties. Keys are strings suitable for use as column headings,
+                    values are functions f(oAbstractCard) -> int which return the column value given an abstract card object.
+        """
+        self._aColNames = aColNames
+        self._dPropFuncs = dPropFuncs
+
+    @staticmethod
+    def getFullAllPropFuncs():
+        """
+        Return a dictionary of card property creation functions.
+        The keys are strings suitable for use as column names, the values are functions for calculating a column entry
+        from an abstract card.
+        Since a new dictionary is created every time this function is called it's safe to edit it to include just
+        the functions you want.
+        
+        For card costs, a cost of X is represented by a value of -1.
+        Many boolean attributes are represented by values of 0 or 1.
+        """
+        d = {}
+        
+        # properties from direct attributes of Abstract Cards
+        d['group'] = lambda card: card.group
+        d['capacity'] = lambda card: card.capacity
+        d['pool cost'] = lambda card: (card.costtype == 'pool' and card.cost) or 0
+        d['blood cost'] = lambda card: (card.costtype == 'blood' and card.cost) or 0
+        d['conviction cost'] = lambda card: (card.costtype == 'conviction' and card.cost) or 0
+        d['advanced'] = lambda card: (card.level == 'advanced' and 1) or 0
+        
+        # discipline properties
+        for oDis in Discipline.select():
+            d['discipline: ' + oDis.name] = lambda card: (oDis in [oPair.discipline for oPair in card.discipline] and 1) or 0
+        
+        # rarity properties
+        for oRar in Rarity.select():
+            d['rarity:' + oRar.name] = lambda card: (oRar in [oPair.rarity for oPair in card.rarity] and 1) or 0
+        
+        # expansion properties
+        for oExp in Expansion.select():
+            d['expansion:' + oExp.name] = lambda card: (oExp in [oPair.rarity for oPair in card.rarity] and 1) or 0
+        
+        # clan properties
+        for oClan in Clan.select():
+            d['clan:' + oClan.name] = lambda card: (oClan in card.clan and 1) or 0
+        
+        # cardtype properties
+        for oType in CardType.select():
+            d['card type:' + oType.name] = lambda card: (oType in card.cardtype and 1) or 0
+            
+        return d
+    
+    def tabulate(self,aCards):
+        """
+        Create a table from the list of (or iterator over) cards.
+        Returns the table which is a nested list where each element is a row and each row consists of column values.
+        The rows are in the same order as the cards in aCards.
+        The columns are in the same order as in the aColNames list passed to __init__.
+        """
+        aColFuncs = [self._dPropFuncs[x] for x in self._aColNames]
+        
+        aTable = []
+        
+        for oC in aCards:
+            oC = IAbstractCard(oC)
+            aRow = []
+            
+            for fProp in aColFuncs:
+                aRow.append(fProp(oC))
+                
+            aTable.append(aRow)
+        
+        return aTable
+    
