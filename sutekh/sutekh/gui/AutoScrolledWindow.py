13e810991f3c6aa38934f9e2d670b80110beb4c9
Simon Cross <hodgestar@gmail.com>
2006-06-30 22:07:09 +0000

    Add option to use a viewport for unscrollable objects.

--- sutekh/sutekh/gui/AutoScrolledWindow.py
+++ sutekh/sutekh/gui/AutoScrolledWindow.py
@@ -5,10 +5,13 @@
 import gtk
 
 class AutoScrolledWindow(gtk.ScrolledWindow,object):
-    def __init__(self,wWidgetToWrap):
+    def __init__(self,wWidgetToWrap,bUseViewport=False):
         super(AutoScrolledWindow,self).__init__()
         
         self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         self.set_shadow_type(gtk.SHADOW_IN)
    
-        self.add(wWidgetToWrap)
+        if bUseViewport:
+            self.add_with_viewport(wWidgetToWrap)
+        else:
+            self.add(wWidgetToWrap)
