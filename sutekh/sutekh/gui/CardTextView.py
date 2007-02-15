492ae54522b6f164185cbad29d0f4849464795c4
Simon Cross <hodgestar@gmail.com>
2007-02-15 21:26:55 +0000

    Use tags to style card text (it's harder than it sounds).

--- sutekh/sutekh/gui/CardTextView.py
+++ sutekh/sutekh/gui/CardTextView.py
@@ -1,77 +1,115 @@
 # CardTextView.py
-# Copyright 2005,2006 Simon Cross <hodgestar@gmail.com>
+# Copyright 2005,2006,2007 Simon Cross <hodgestar@gmail.com>
 # Copyright 2006 Neil Muller <drnlmuller+sutekh@gmail.com>
 # GPL - see COPYING for details
 
 import gtk
+import pango
+
+class CardTextBuffer(gtk.TextBuffer,object):
+    def __init__(self):
+        super(CardTextBuffer,self).__init__(None)
+        tags = []
+
+        # See http://www.pygtk.org/pygtk2reference/class-gtktexttag.html
+        # for some possible properties
+
+        tags.append(self.create_tag("label",underline=pango.UNDERLINE_SINGLE))
+
+        tags.append(self.create_tag("card_name",weight=pango.WEIGHT_BOLD))
+        tags.append(self.create_tag("cost",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("capacity",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("group",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("level",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("card_type",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("clan",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("discipline",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("expansion",style=pango.STYLE_ITALIC))
+        tags.append(self.create_tag("ruling"))
+        tags.append(self.create_tag("card_text",style=pango.STYLE_ITALIC))
+
+    def tagText(self,*args,**kwargs):
+        self.insert_with_tags_by_name(self._oIter,*args,**kwargs)
+
+    def labelledValue(self,sLabel,sValue,sTag):
+        self.tagText("\n")
+        self.tagText(sLabel,"label")
+        self.tagText(": ")
+        self.tagText(sValue,sTag)
+
+    def labelledList(self,sLabel,aValues,sTag):
+        self.tagText("\n")
+        self.tagText(sLabel,"label")
+        self.tagText(":")
+        for sValue in aValues:
+            self.tagText("\n\t* ")
+            self.tagText(sValue,sTag)
+
+    def resetIter(self):
+        self._oIter = self.get_iter_at_offset(0)
 
 class CardTextView(gtk.TextView,object):
     def __init__(self,oController):
         super(CardTextView,self).__init__()
         self.__oC = oController
-        self.__oBuf = gtk.TextBuffer(None)
-        
+        self.__oBuf = CardTextBuffer()
+
         self.set_buffer(self.__oBuf)
         self.set_editable(False)
         self.set_cursor_visible(False)
-        self.set_wrap_mode(2)
+        self.set_wrap_mode(gtk.WRAP_WORD)
 
         self.set_size_request(-1,250)
-        
+
     def setCardText(self,oCard):
         oStart, oEnd = self.__oBuf.get_bounds()
         self.__oBuf.delete(oStart,oEnd)
-                
-        oIter = self.__oBuf.get_iter_at_offset(0)
-        self.__oBuf.insert(oIter,self.printCard(oCard))
-        
-    def printCard(self,oCard):
-        s = oCard.name
-        
+        self.printCardToBuffer(oCard,self.__oBuf)
+
+    def printCardToBuffer(self,oCard,oBuf):
+        oBuf.resetIter()
+
+        oBuf.tagText(oCard.name,"card_name")
+
         if not oCard.cost is None:
             if oCard.cost == -1:
-                s += "\nCost: X " + str(oCard.costtype)
+                sCost = "X " + str(oCard.costtype)
             else:
-                s += "\nCost: " + str(oCard.cost) + " " + str(oCard.costtype)
-        
+                sCost = str(oCard.cost) + " " + str(oCard.costtype)
+            oBuf.labelledValue("Cost",sCost,"cost")
+
         if not oCard.capacity is None:
-            s += "\nCapacity: " + str(oCard.capacity)
-        
+            oBuf.labelledValue("Capacity",str(oCard.capacity),"capacity")
+
         if not oCard.group is None:
-            s += "\nGroup: " + str(oCard.group)
-            
+            oBuf.labelledValue("Group",str(oCard.group),"group")
+
         if not oCard.level is None:
-            s += "\nLevel: " + str(oCard.level)
-        
-        s += "\nCard Type:"
+            oBuf.labelledValue("Level",str(oCard.level),"level")
+
         if len(oCard.cardtype) == 0:
-            s += "\n\t* Unknown"
-        for oT in oCard.cardtype:
-            s += "\n\t* " + oT.name
-        
+            aTypes = ["Unknown"]
+        else:
+            aTypes = [oT.name for oT in oCard.cardtype]
+        oBuf.labelledList("Card Type",aTypes,"card_type")
+
         if not len(oCard.clan) == 0:
-            s += "\nClan:"
-        for oC in oCard.clan:
-            s += "\n\t* " + oC.name
-        
+            aClans = [oC.name for oC in oCard.clan]
+            oBuf.labelledList("Clan",aClans,"clan")
+
         if not len(oCard.discipline) == 0:
-            s += "\nDisciplines:"
-        for oP in oCard.discipline:
-            if oP.level == 'superior':
-                s += "\n\t* " + oP.discipline.name.upper()
-            else:
-                s += "\n\t* " + oP.discipline.name
-                
+            aDis = []
+            aDis.extend([oP.discipline.name.upper() for oP in oCard.discipline if oP.level == 'superior'])
+            aDis.extend([oP.discipline.name for oP in oCard.discipline if oP.level != 'superior'])
+            oBuf.labelledList("Disciplines",aDis,"discipline")
+
         if not len(oCard.rarity) == 0:
-            s += "\nExpansions:"
-        for oP in oCard.rarity:
-            s += "\n\t* " + oP.expansion.name + " (" + oP.rarity.name + ")"
-        
+            aExp = [oP.expansion.name + " (" + oP.rarity.name + ")" for oP in oCard.rarity]
+            oBuf.labelledList("Expansions",aExp,"expansion")
+
         if not len(oCard.rulings) == 0:
-            s += "\nRulings:"
-        for oR in oCard.rulings:
-            s += "\n\t* " + oR.text.replace("\n"," ") + " " + oR.code
-            
-        s += "\n\n" + oCard.text.replace("\n"," ")
-        
-        return s
+            aRulings = [oR.text.replace("\n"," ") + " " + oR.code for oR in oCard.rulings]
+            oBuf.labelledList("Rulings",aRulings,"ruling")
+
+        oBuf.tagText("\n\n")
+        oBuf.tagText(oCard.text.replace("\n"," "),"card_text")
