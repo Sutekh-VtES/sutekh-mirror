ef0199b7d09ffa4c5cce667e3aace765e628ba59
Simon Cross <hodgestar@gmail.com>
2007-03-20 09:51:54 +0000

    This is version 0.3.2.

--- sutekh/setup.py
+++ sutekh/setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 class SutekhInfo(object):
-    VERSION = (0,3,1)
+    VERSION = (0,3,2)
     VERSION_STR = '.'.join([str(x) for x in VERSION])
 
     NAME = 'Sutekh'
