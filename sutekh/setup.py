db9682affc26a72b8459bcaf7e33c7e6fff00344
Simon Cross <hodgestar@gmail.com>
2007-05-06 15:49:42 +0000

    Move SutekhInfo into sutekh package.

--- sutekh/setup.py
+++ sutekh/setup.py
@@ -1,41 +1,12 @@
 # setup.py
 # Copyright 2007 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
+
+# Be sure that the import will grab the correct version
+# of SutekhInfo when building packages.
 
 from setuptools import setup, find_packages
-
-class SutekhInfo(object):
-    VERSION = (0,3,2)
-    VERSION_STR = '.'.join([str(x) for x in VERSION])
-
-    NAME = 'Sutekh'
-    DESCRIPTION = 'VtES Card Collection Manager'
-
-    AUTHORS = [ ('Simon Cross','hodgestar+sutekh@gmail.com'),
-                ('Neil Muller','drnmuller+sutekh@gmail.com') ]
-
-    AUTHOR_NAME = AUTHORS[0][0]
-    AUTHOR_EMAIL = AUTHORS[0][1]
-
-    MAINTAINERS = AUTHORS
-
-    MAINTAINER_NAME = MAINTAINERS[0][0]
-    MAINTAINER_EMAIL = MAINTAINERS[0][1]
-
-    SOURCEFORGE_URL = 'http://sourceforge.net/projects/sutekh/'
-
-    LICENSE = 'GPL'
-
-    INSTALL_REQUIRES = [
-        'SQLObject',
-        'PyProtocols'
-    ]
-
-    # Install these manually
-    NON_EGG_REQUIREMENTS = [
-        'pysqlite',
-        'PyGTK',
-        'PyXML'
-    ]
+from sutekh.SutekhInfo import SutekhInfo
 
 setup   (   # Metadata
             name = SutekhInfo.NAME,
---
+++ sutekh/sutekh/SutekhInfo.py
@@ -0,0 +1,38 @@
+# SutekhInfo.py
+# Copyright 2007 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
+
+class SutekhInfo(object):
+    VERSION = (0,3,2)
+    VERSION_STR = '.'.join([str(x) for x in VERSION])
+
+    NAME = 'Sutekh'
+    DESCRIPTION = 'VtES Card Collection Manager'
+
+    AUTHORS = [ ('Simon Cross','hodgestar+sutekh@gmail.com'),
+                ('Neil Muller','drnmuller+sutekh@gmail.com') ]
+
+    AUTHOR_NAME = AUTHORS[0][0]
+    AUTHOR_EMAIL = AUTHORS[0][1]
+
+    MAINTAINERS = AUTHORS
+
+    MAINTAINER_NAME = MAINTAINERS[0][0]
+    MAINTAINER_EMAIL = MAINTAINERS[0][1]
+
+    SOURCEFORGE_URL = 'http://sourceforge.net/projects/sutekh/'
+
+    LICENSE = 'GPL'
+    LICENSE_TEXT = "TODO: Read this from the COPYING file."
+
+    INSTALL_REQUIRES = [
+        'SQLObject',
+        'PyProtocols'
+    ]
+
+    # Install these manually
+    NON_EGG_REQUIREMENTS = [
+        'pysqlite',
+        'PyGTK',
+        'PyXML'
+    ]
