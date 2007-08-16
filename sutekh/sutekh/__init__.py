336418101ea1c97bb6e41eac90a1f5d453d9e464
Simon Cross <hodgestar@gmail.com>
2007-08-16 21:01:28 +0000

    Re-organise __all__ and remove 'import *'s to make Pyflakes happy and guard against typos in __all__.

--- sutekh/sutekh/__init__.py
+++ sutekh/sutekh/__init__.py
@@ -2,29 +2,52 @@
 # Copyright 2005,2006 Simon Cross <hodgestar@gmail.com>
 # GPL - see COPYING for details
 
-__all__ = [ # Sutekh Objects
-            'AbstractCard', 'PhysicalCard', 'AbstractCardSet', 'PhysicalCardSet',
-            'RarityPair', 'Expansion', 'Rarity', 'DisciplinePair', 'Discipline',
-            'Clan', 'CardType', 'Ruling',
-            # Filters
-            'FilterAndBox', 'FilterOrBox', 'ClanFilter', 'DisciplineFilter',
-            'CardTypeFilter', 'CardTextFilter', 'MultiCardTypeFilter',
-            'MultiDisciplineFilter', 'MultiClanFilter', 'PhysicalCardSetFilter',
-            'PhysicalCardFilter', 'GroupFilter', 'MultiGroupFilter',
-            'ExpansionFilter', 'MultiExpansionFilter', 'AbstractCardSetFilter',
-            # Groupings
-            'CardTypeGrouping', 'ClanGrouping', 'DisciplineGrouping',
-            'ExpansionGrouping', 'RarityGrouping',
-            # Misc
-           'start', 'CardListTabulator' ]
-
 # Do importing
-from sutekh.SutekhObjects import *
-from sutekh.Filters import *
-from sutekh.Groupings import *
+
+from sutekh.SutekhObjects import AbstractCard, PhysicalCard, \
+                    AbstractCardSet, PhysicalCardSet, RarityPair, \
+                    Expansion, Rarity, DisciplinePair, Discipline, \
+                    Clan, CardType, Ruling
+
+from sutekh.Filters import FilterAndBox, FilterOrBox, ClanFilter, \
+                    DisciplineFilter, CardTypeFilter, CardTextFilter, \
+                    MultiCardTypeFilter, MultiDisciplineFilter, \
+                    MultiClanFilter, PhysicalCardSetFilter, \
+                    PhysicalCardFilter, GroupFilter, MultiGroupFilter, \
+                    ExpansionFilter, MultiExpansionFilter, \
+                    AbstractCardSetFilter
+
+from sutekh.Groupings import CardTypeGrouping, ClanGrouping, \
+                    DisciplineGrouping, ExpansionGrouping, \
+                    RarityGrouping
+
 from sutekh.CardListTabulator import CardListTabulator
+
 from sutekh.SutekhCli import main
 
 # start() method for use when working in the Python interpreter
+
 def start(aArgs=['sutekh']):
     main(aArgs)
+
+# What we expose to import *
+
+aAll = [ # Sutekh Objects
+         AbstractCard, PhysicalCard, AbstractCardSet, PhysicalCardSet,
+         RarityPair, Expansion, Rarity, DisciplinePair, Discipline,
+         Clan, CardType, Ruling,
+         # Filters
+         FilterAndBox, FilterOrBox, ClanFilter, DisciplineFilter,
+         CardTypeFilter, CardTextFilter, MultiCardTypeFilter,
+         MultiDisciplineFilter, MultiClanFilter, PhysicalCardSetFilter,
+         PhysicalCardFilter, GroupFilter, MultiGroupFilter,
+         ExpansionFilter, MultiExpansionFilter, AbstractCardSetFilter,
+         # Groupings
+         CardTypeGrouping, ClanGrouping, DisciplineGrouping,
+         ExpansionGrouping, RarityGrouping,
+         # Misc
+         start, CardListTabulator ]
+
+__all__ = [ x.__name__ for x in aAll ]
+
+
