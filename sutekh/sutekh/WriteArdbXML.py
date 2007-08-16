29cd0c03d724dbeed26656d153af896f954f31cc
Simon Cross <hodgestar@gmail.com>
2007-08-16 21:37:23 +0000

    Put spaces around = in assignments.

--- sutekh/sutekh/WriteArdbXML.py
+++ sutekh/sutekh/WriteArdbXML.py
@@ -24,7 +24,7 @@ class WriteArdbXML(object):
         oDoc = getDOMImplementation().createDocument(None,'deck',None)
 
         oDeckElem = oDoc.firstChild
-        sDateWritten=time.strftime('%Y-%m-%d',time.localtime())
+        sDateWritten = time.strftime('%Y-%m-%d',time.localtime())
         oDeckElem.setAttribute('generator',"Sutekh [pre-release]")
         oDeckElem.setAttribute('formatVersion',"-TODO-1.0") # Claim same version as recent ARDB
         oDeckElem.setAttribute('databaseVersion',"Sutekh-20070701")
@@ -41,8 +41,8 @@ class WriteArdbXML(object):
         oDateElem.appendChild(oDoc.createTextNode(sDateWritten))
         oDeckElem.appendChild(oDateElem)
 
-        (dVamps,iCryptSize,iMin,iMax,fAvg)=self.extractCrypt(dCards)
-        (dLib,iLibSize)=self.extractLibrary(dCards)
+        (dVamps,iCryptSize,iMin,iMax,fAvg) = self.extractCrypt(dCards)
+        (dLib,iLibSize) = self.extractLibrary(dCards)
 
         oCryptElem = oDoc.createElement('crypt')
         oCryptElem.setAttribute('size',str(iCryptSize))
@@ -52,7 +52,7 @@ class WriteArdbXML(object):
         oDeckElem.appendChild(oCryptElem)
         for tKey, iNum in dVamps.iteritems():
             iId, sName = tKey
-            oCard=IAbstractCard(sName)
+            oCard = IAbstractCard(sName)
             oCardElem = oDoc.createElement('vampire')
             # This won't match the ARDB ID's, unless by chance.
             # It looks like that should not be an issue as ARDB will
@@ -61,8 +61,8 @@ class WriteArdbXML(object):
             oCardElem.setAttribute('count',str(iNum))
             # It's unclear to me what values ARDB uses here, but
             # these are fine for the xml2html conversion, and look meaningful
-            oAdvElem=oDoc.createElement('adv')
-            oNameElem=oDoc.createElement('name')
+            oAdvElem = oDoc.createElement('adv')
+            oNameElem = oDoc.createElement('name')
             if oCard.level is not None:
                 oAdvElem.appendChild(oDoc.createTextNode("(Advanced)"))
                 # This is a bit hackish
@@ -72,12 +72,12 @@ class WriteArdbXML(object):
                 oNameElem.appendChild(oDoc.createTextNode(sName))
             oCardElem.appendChild(oAdvElem)
             oCardElem.appendChild(oNameElem)
-            oDiscElem=oDoc.createElement('disciplines')
-            sDisciplines=self.getDisc(oCard)
+            oDiscElem = oDoc.createElement('disciplines')
+            sDisciplines = self.getDisc(oCard)
             oDiscElem.appendChild(oDoc.createTextNode(sDisciplines))
             oCardElem.appendChild(oDiscElem)
-            aClan=[x.name for x in oCard.clan]
-            oClanElem=oDoc.createElement('clan')
+            aClan = [x.name for x in oCard.clan]
+            oClanElem = oDoc.createElement('clan')
             oClanElem.appendChild(oDoc.createTextNode(aClan[0]))
             oCardElem.appendChild(oClanElem)
             oCapElem=oDoc.createElement('capacity')
@@ -91,11 +91,11 @@ class WriteArdbXML(object):
             # this seems set when the ARDB database is created, and is
             # not in the ARDB codebase
             if len(oCard.title)>0:
-                oTitleElem=oDoc.createElement('title')
-                aTitles=[oC.name for oC in oCard.title]
+                oTitleElem = oDoc.createElement('title')
+                aTitles = [oC.name for oC in oCard.title]
                 oTitleElem.appendChild(oDoc.createTextNode(aTitles[0]))
                 oCardElem.appendChild(oTitleElem)
-            oTextElem=oDoc.createElement('text')
+            oTextElem = oDoc.createElement('text')
             oTextElem.appendChild(oDoc.createTextNode(oCard.text))
             oCardElem.appendChild(oTextElem)
             oCryptElem.appendChild(oCardElem)
@@ -109,13 +109,13 @@ class WriteArdbXML(object):
             oCardElem = oDoc.createElement('card')
             oCardElem.setAttribute('databaseID',str(iId))
             oCardElem.setAttribute('count',str(iNum))
-            oNameElem=oDoc.createElement('name')
+            oNameElem = oDoc.createElement('name')
             oNameElem.appendChild(oDoc.createTextNode(sName))
             oCardElem.appendChild(oNameElem)
             if oCard.costtype is not None:
-                oCostElem=oDoc.createElement('cost')
-                oCostElem.appendChild(oDoc.createTextNode( \
-                        str(oCard.cost)+" "+oCard.costtype))
+                oCostElem = oDoc.createElement('cost')
+                oCostElem.appendChild(oDoc.createTextNode(
+                        str(oCard.cost) + " " + oCard.costtype))
                 oCardElem.appendChild(oCostElem)
             if len(oCard.clan)>0:
                 # ARDB also strores things like "requires a prince"
@@ -125,18 +125,18 @@ class WriteArdbXML(object):
                 oReqElem.appendChild(oDoc.createTextNode("/".join(aClan)))
                 oCardElem.appendChild(oReqElem)
             # Looks like it should be the right thing, but may not
-            aTypes=[x.name for x in oCard.cardtype]
-            sType="/".join(aTypes)
-            oTypeElem=oDoc.createElement('type')
+            aTypes = [x.name for x in oCard.cardtype]
+            sType = "/".join(aTypes)
+            oTypeElem = oDoc.createElement('type')
             oTypeElem.appendChild(oDoc.createTextNode(sType))
             oCardElem.appendChild(oTypeElem)
             # Not sure if this does quite the right thing here
             sDisciplines=self.getDisc(oCard)
-            if sDisciplines!='':
-                oDiscElem=oDoc.createElement('disciplines')
+            if sDisciplines != '':
+                oDiscElem = oDoc.createElement('disciplines')
                 oDiscElem.appendChild(oDoc.createTextNode(sDisciplines))
                 oCardElem.appendChild(oDiscElem)
-            oTextElem=oDoc.createElement('text')
+            oTextElem = oDoc.createElement('text')
             oTextElem.appendChild(oDoc.createTextNode(oCard.text))
             oCardElem.appendChild(oTextElem)
             oLibElem.appendChild(oCardElem)
@@ -147,7 +147,7 @@ class WriteArdbXML(object):
         Takes filename, deck details and a dictionary of cards, of the form
         dCard[(id,name)]=count
         """
-        oDoc=self.genDoc(sSetName,sAuthor,sDescription,dCards)
+        oDoc = self.genDoc(sSetName,sAuthor,sDescription,dCards)
         PrettyPrint(oDoc,fOut)
 
     def getDisc(self,oCard):
@@ -164,37 +164,37 @@ class WriteArdbXML(object):
             return ""
 
     def extractCrypt(self,dCards):
-        iCryptSize=0
-        iMax=0
-        iMin=75
-        fAvg=0.0
-        dVamps={}
+        iCryptSize = 0
+        iMax = 0
+        iMin = 75
+        fAvg = 0.0
+        dVamps = {}
         for tKey,iCount in dCards.iteritems():
             iId,sName = tKey
-            oCard=IAbstractCard(sName)
-            aTypes=[x.name for x in oCard.cardtype]
-            if aTypes[0]=='Vampire':
-                dVamps[tKey]=iCount
-                iCryptSize+=iCount
-                fAvg+=oCard.capacity*iCount
+            oCard = IAbstractCard(sName)
+            aTypes = [x.name for x in oCard.cardtype]
+            if aTypes[0] == 'Vampire':
+                dVamps[tKey] = iCount
+                iCryptSize += iCount
+                fAvg += oCard.capacity*iCount
                 if oCard.capacity>iMax:
-                    iMax=oCard.capacity
+                    iMax = oCard.capacity
                 if oCard.capacity<iMin:
-                    iMin=oCard.capacity
+                    iMin = oCard.capacity
         if iCryptSize>0:
-            fAvg=round(fAvg/iCryptSize,2)
-        if iMin==75:
-            iMin=0
+            fAvg = round(fAvg/iCryptSize,2)
+        if iMin == 75:
+            iMin = 0
         return (dVamps,iCryptSize,iMin,iMax,fAvg)
 
     def extractLibrary(self,dCards):
-        iSize=0
-        dLib={}
+        iSize = 0
+        dLib = {}
         for tKey,iCount in dCards.iteritems():
             iId,sName = tKey
-            oCard=IAbstractCard(sName)
-            aTypes=[x.name for x in oCard.cardtype]
-            if aTypes[0]!='Vampire':
-                dLib[tKey]=iCount
-                iSize+=iCount
+            oCard = IAbstractCard(sName)
+            aTypes = [x.name for x in oCard.cardtype]
+            if aTypes[0] != 'Vampire':
+                dLib[tKey] = iCount
+                iSize += iCount
         return (dLib,iSize)
