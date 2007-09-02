35f812576c4cb8784db56e1f24363d57fe554191
Neil Muller <drnlmuller+sutekh@gmail.com>
2007-09-02 20:17:48 +0000

    Some style cleanups in CellRenderer. Add note about keyboard shortcuts to TODO

--- sutekh/sutekh/gui/CellRendererSutekhButton.py
+++ sutekh/sutekh/gui/CellRendererSutekhButton.py
@@ -17,55 +17,57 @@ import gtk, gobject
 class CellRendererSutekhButton(gtk.GenericCellRenderer):
     def __init__(self):
         self.__gobject_init__()
-        self.pixbuf = None
-        self.set_property("mode",gtk.CELL_RENDERER_MODE_ACTIVATABLE)
+        self.oPixbuf = None
+        self.set_property("mode", gtk.CELL_RENDERER_MODE_ACTIVATABLE)
 
-    def load_icon(self,name,widget):
+    def load_icon(self, sName, oWidget):
         # Load the icon specified in name
-        self.pixbuf = widget.render_icon(name,gtk.ICON_SIZE_SMALL_TOOLBAR)
+        self.oPixbuf = oWidget.render_icon(sName, gtk.ICON_SIZE_SMALL_TOOLBAR)
 
 
-    def on_get_size(self,widget,cell_area):
-        if self.pixbuf == None:
+    def on_get_size(self, oWidget, oCellArea):
+        if self.oPixbuf is None:
             return 0, 0, 0, 0
-        pixbuf_width  = self.pixbuf.get_width()
-        pixbuf_height = self.pixbuf.get_height()
-        calc_width  = self.get_property("xpad") * 2 + pixbuf_width
-        calc_height = self.get_property("ypad") * 2 + pixbuf_height
-        x_offset = 0
-        y_offset = 0
-        if cell_area and pixbuf_width > 0 and pixbuf_height > 0:
-            x_offset = self.get_property("xalign") * (cell_area.width - \
-                calc_width -  self.get_property("xpad"))
-            y_offset = self.get_property("yalign") * (cell_area.height - \
-                calc_height -  self.get_property("ypad"))
-        return x_offset, y_offset, calc_width, calc_height
+        iPixbufWidth  = self.oPixbuf.get_width()
+        iPixbufHeight = self.oPixbuf.get_height()
+        iCalcWidth  = self.get_property("xpad") * 2 + iPixbufWidth
+        iCalcHeight = self.get_property("ypad") * 2 + iPixbufHeight
+        iXOffset = 0
+        iYOffset = 0
+        if oCellArea is not None and iPixbufWidth > 0 and iPixbufHeight > 0:
+            iXOffset = self.get_property("xalign") * (oCellArea.width - \
+                iCalcWidth -  self.get_property("xpad"))
+            iYOffset = self.get_property("yalign") * (oCellArea.height - \
+                iCalcHeight -  self.get_property("ypad"))
+        return iXOffset, iYOffset, iCalcWidth, iCalcHeight
 
-    def on_activate(self, event, widget, path, background_area, cell_area, flags):
+    def on_activate(self, oEvent, oWidget, oPath, oBackgroundArea,
+            oCellArea, iFlags):
         # TODO - setup drawing of clicked image
         # clicked callback should be called
-        self.emit('clicked',path)
+        self.emit('clicked', oPath)
         return True
 
-    def on_render(self,window, widget, background_area, cell_area, expose_area, flags):
-        if (self.pixbuf == None):
+    def on_render(self, oWindow, oWidget, oBackgroundArea,
+            oCellArea, oExposeArea, iFlags):
+        if self.oPixbuf is None:
             return None
         # Render pixbuf
-        pix_rect = gtk.gdk.Rectangle()
-        pix_rect.x, pix_rect.y, pix_rect.width, pix_rect.height = \
-            self.on_get_size(widget, cell_area)
+        oPixRect = gtk.gdk.Rectangle()
+        oPixRect.x, oPixRect.y, oPixRect.width, oPixRect.height = \
+            self.on_get_size(oWidget, oCellArea)
 
-        pix_rect.x += cell_area.x
-        pix_rect.y += cell_area.y
-        pix_rect.width  -= 2 * self.get_property("xpad")
-        pix_rect.height -= 2 * self.get_property("ypad")
+        oPixRect.x += oCellArea.x
+        oPixRect.y += oCellArea.y
+        oPixRect.width  -= 2 * self.get_property("xpad")
+        oPixRect.height -= 2 * self.get_property("ypad")
 
-        draw_rect = cell_area.intersect(pix_rect)
-        draw_rect = expose_area.intersect(draw_rect)
+        oDrawRect = oCellArea.intersect(oPixRect)
+        oDrawRect = oExposeArea.intersect(oDrawRect)
 
-        window.draw_pixbuf(widget.style.black_gc, self.pixbuf, \
-            draw_rect.x-pix_rect.x, draw_rect.y-pix_rect.y, draw_rect.x, \
-            draw_rect.y+2, draw_rect.width, draw_rect.height, \
+        oWindow.draw_pixbuf(oWidget.style.black_gc, self.oPixbuf,
+            oDrawRect.x - oPixRect.x, oDrawRect.y - oPixRect.y, oDrawRect.x,
+            oDrawRect.y + 2, oDrawRect.width, oDrawRect.height,
             gtk.gdk.RGB_DITHER_NONE, 0, 0)
         return None
 
@@ -80,8 +82,8 @@ gobject.type_register(CellRendererSutekhButton)
 # Setup clicked signal -- can also be done using the __gsignals__
 # dict in the class, but I couldn't find good documentation for
 # that approach.
-gobject.signal_new("clicked", CellRendererSutekhButton,\
-    gobject.SIGNAL_RUN_FIRST|gobject.SIGNAL_ACTION,\
+gobject.signal_new("clicked", CellRendererSutekhButton,
+    gobject.SIGNAL_RUN_FIRST|gobject.SIGNAL_ACTION,
     gobject.TYPE_NONE,
     (gobject.TYPE_STRING,))
-# the callback is called as callback(self,path)
+# the callback is called as callback (self, oPath)
