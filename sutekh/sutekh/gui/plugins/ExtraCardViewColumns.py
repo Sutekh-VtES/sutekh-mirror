c4510879ead1f577c96d72298b721f040b46273f
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-02-18 15:58:19 +0000

    Allow extra columns to be used for sorting card list view

--- sutekh/sutekh/gui/plugins/ExtraCardViewColumns.py
+++ sutekh/sutekh/gui/plugins/ExtraCardViewColumns.py
@@ -6,7 +6,7 @@ import gtk, pango
 from sutekh.gui.PluginManager import CardListPlugin
 from sutekh.gui.SutekhDialog import SutekhDialog
 from sutekh.core.SutekhObjects import AbstractCard, PhysicalCard, \
-                                      AbstractCardSet, PhysicalCardSet
+                                      AbstractCardSet, PhysicalCardSet, IAbstractCard
 from sqlobject import SQLObjectNotFound
 
 class ExtraCardViewColumns(CardListPlugin):
@@ -23,68 +23,102 @@ class ExtraCardViewColumns(CardListPlugin):
         self._dCols['Group'] = self._renderGroup
         self._dCols['Capacity'] = self._renderCapacity
 
+        self._dSortDataFuncs = {}
+        self._dSortDataFuncs['Card Type'] = self._get_data_cardtype
+        self._dSortDataFuncs['Clan'] = self._get_data_clan
+        self._dSortDataFuncs['Disciplines'] = self._get_data_disciplines
+        self._dSortDataFuncs['Expansions'] = self._get_data_expansions
+        self._dSortDataFuncs['Group'] = self._get_data_group
+        self._dSortDataFuncs['Capacity'] = self._get_data_capacity
+
+        self._dCardCache = {} # Used for sorting
+
     # Rendering Functions
 
     def _getCard(self,oIter):
         if self.model.iter_depth(oIter) == 1:
             # Only try and lookup things that look like they should be cards
             try:
-                oCard = AbstractCard.byCanonicalName(self.model.getNameFromIter(oIter).lower())
-                return oCard
+                sCardName = self.model.getNameFromIter(oIter).lower()
+                # Cache lookups, so we don't hit the database so hard when sorting
+                if not self._dCardCache.has_key(sCardName):
+                     self._dCardCache[sCardName] = IAbstractCard(sCardName)
+                return self._dCardCache[sCardName]
             except SQLObjectNotFound:
                 return None
         else:
             return None
 
-    def _renderCardType(self,oColumn,oCell,oModel,oIter):
-        oCard = self._getCard(oIter)
+    def _get_data_cardtype(self, oCard):
         if not oCard is None:
             aTypes = [x.name for x in oCard.cardtype]
             aTypes.sort()
-            oCell.set_property("text", ",".join(aTypes))
-        else:
-            oCell.set_property("text","")
+            return ",".join(aTypes)
+        return ""
 
-    def _renderClan(self,oColumn,oCell,oModel,oIter):
+    def _renderCardType(self,oColumn,oCell,oModel,oIter):
         oCard = self._getCard(oIter)
+        oCell.set_property("text", self._get_data_cardtype(oCard))
+
+    def _get_data_clan(self, oCard):
         if not oCard is None:
             aClans = [x.name for x in oCard.clan]
             aClans.sort()
-            oCell.set_property("text", ",".join(aClans))
-        else:
-            oCell.set_property("text","")
+            return ",".join(aClans)
+        return ""
 
-    def _renderDisciplines(self,oColumn,oCell,oModel,oIter):
+    def _renderClan(self,oColumn,oCell,oModel,oIter):
         oCard = self._getCard(oIter)
+        oCell.set_property("text", self._get_data_clan(oCard))
+
+    def _get_data_disciplines(self, oCard):
         if not oCard is None:
             aDis = [(oP.level != 'superior') and oP.discipline.name or oP.discipline.name.upper() for oP in oCard.discipline]
             aDis.sort()
-            oCell.set_property("text",",".join(aDis))
-        else:
-            oCell.set_property("text","")
+            return ",".join(aDis)
+        return ""
 
-    def _renderExpansions(self,oColumn,oCell,oModel,oIter):
+    def _renderDisciplines(self,oColumn,oCell,oModel,oIter):
         oCard = self._getCard(oIter)
+        oCell.set_property("text", self._get_data_disciplines(oCard))
+
+    def _get_data_expansions(self, oCard):
         if not oCard is None:
             aExp = [oP.expansion.shortname + "(" + oP.rarity.name + ")" for oP in oCard.rarity]
             aExp.sort()
-            oCell.set_property("text",",".join(aExp))
-        else:
-            oCell.set_property("text","")
+            return ",".join(aExp)
+        return ""
+
+    def _renderExpansions(self,oColumn,oCell,oModel,oIter):
+        oCard = self._getCard(oIter)
+        oCell.set_property("text", self._get_data_expansions(oCard))
+
+    def _get_data_group(self, oCard):
+        if not oCard is None and not oCard.group is None:
+            return oCard.group
+        return -1
 
     def _renderGroup(self,oColumn,oCell,oModel,oIter):
         oCard = self._getCard(oIter)
-        if not oCard is None and not oCard.group is None:
-            oCell.set_property("text",str(oCard.group))
+        iGrp = self._get_data_group(oCard)
+        if iGrp != -1:
+            oCell.set_property("text", str(iGrp))
         else:
-            oCell.set_property("text","")
+            oCell.set_property("text", "")
+
+    def _get_data_capacity(self, oCard):
+        if not oCard is None and not oCard.capacity is None:
+            return oCard.capacity
+        return -1
 
     def _renderCapacity(self,oColumn,oCell,oModel,oIter):
         oCard = self._getCard(oIter)
-        if not oCard is None and not oCard.capacity is None:
-            oCell.set_property("text",str(oCard.capacity))
+        iCap = self._get_data_capacity(oCard)
+        if iCap != -1:
+            oCell.set_property("text", str(iCap))
         else:
-            oCell.set_property("text","")
+            oCell.set_property("text", "")
+
 
     # Dialog and Menu Item Creation
 
@@ -142,13 +176,39 @@ class ExtraCardViewColumns(CardListPlugin):
             oDlg.destroy()
 
     def setColsInUse(self,aCols):
+        iSortCol, iDir = self.model.get_sort_column_id()
+        if iSortCol is not None and iSortCol > 1:
+            # We're changing the columns, so restore sorting to default
+            self.model.set_sort_column_id(0, 0) 
+
         for oCol in self._getColObjects():
             self.view.remove_column(oCol)
+
 
         for i, sCol in enumerate(aCols):
             oCell = gtk.CellRendererText()
             oCell.set_property('style', pango.STYLE_ITALIC)
-            self.view.insert_column_with_data_func(i + 2,sCol,oCell,self._dCols[sCol])
+            oColumn = gtk.TreeViewColumn(sCol, oCell)
+            oColumn.set_cell_data_func(oCell, self._dCols[sCol])
+            self.view.insert_column(oColumn, i+2)
+            oColumn.set_sort_column_id(i+2)
+            self.model.set_sort_func(i+2, self.sort_column, self._dSortDataFuncs[sCol])
+
+    def sort_column(self, oModel, oIter1, oIter2, oGetData):
+        """
+        Stringwise comparision of oIter1 and oIter2. 
+        Return -1 if oIter1 < oIter, 0 in ==, 1 if >
+        """
+        oCard1 = self._getCard(oIter1)
+        oCard2 = self._getCard(oIter2)
+        oVal1 = oGetData(oCard1)
+        oVal2 = oGetData(oCard2)
+        if oVal1 < oVal2:
+            return -1
+        elif oVal1 > oVal2:
+            return 1
+        else:
+            return 0
 
     def getColsInUse(self):
         return [oCol.get_property("title") for oCol in self._getColObjects()]
