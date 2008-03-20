ffa03327ab05243b1a68b8679ce7bf4440330f6d
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-03-20 11:57:11 +0000

    pylint cleanup

--- sutekh/sutekh/gui/plugins/PhysicalCardSetFromAbstract.py
+++ sutekh/sutekh/gui/plugins/PhysicalCardSetFromAbstract.py
@@ -1,12 +1,15 @@
 # PhysicalCardSetFromAbstract.py
 # -*- coding: utf8 -*-
 # vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
-# Copyright 2006 Simon Cross <hodgestar@gmail.com>, Neil Muller <drnlmuller+sutekh@gmail.com>
+# Copyright 2006 Simon Cross <hodgestar@gmail.com>,
+# Copyright 2006 Neil Muller <drnlmuller+sutekh@gmail.com>
 # GPL - see COPYING for details
 
+"Convert an Abstract Card Set to a Physical Card Set"
+
 import gtk
-from sutekh.core.SutekhObjects import PhysicalCardSet, AbstractCardSet, PhysicalCard, \
-                                      IAbstractCard
+from sutekh.core.SutekhObjects import PhysicalCardSet, AbstractCardSet, \
+        PhysicalCard, IAbstractCard
 from sutekh.gui.SutekhDialog import do_complaint_error
 from sutekh.gui.CreateCardSetDialog import CreateCardSetDialog
 from sutekh.gui.PluginManager import CardListPlugin
@@ -15,10 +18,12 @@ class PhysicalCardSetFromAbstract(CardListPlugin):
     """Create a (as far as possible) equivilant Physical Card Set
        from a given Abstract Card Set.
 
-       - Ignores Cards which don't exist in Physical Cards (but notifies the user)
-       - Has an option to import the cards into your card collection before creating the Physical Card Set."""
+       - Ignores Cards which don't exist in Physical Cards
+         (but notifies the user)
+       - Has an option to import the cards into your card collection
+         before creating the Physical Card Set."""
 
-    dTableVersions = { PhysicalCardSet: [2,3,4], PhysicalCard: [1,2] }
+    dTableVersions = { PhysicalCardSet: [2, 3, 4], PhysicalCard: [1, 2] }
     aModelsSupported = [AbstractCardSet]
 
     def get_menu_item(self):
@@ -32,19 +37,31 @@ class PhysicalCardSetFromAbstract(CardListPlugin):
         return iDF
 
     def get_desired_menu(self):
+        "Override base class. Register on the plugins menu"
         return "Plugins"
 
-    def activate(self,oWidget):
-        self.createPhysCardSet()
+    # pylint: disable-msg=W0613
+    # oWidget required by gtk function signature
+    def activate(self, oWidget):
+        "Handle menu activation"
+        self.create_physical_card_set()
+    # pylint: enable-msg=W0613
 
-    def createPhysCardSet(self):
+    def create_physical_card_set(self):
+        "Create the actual physical card"
+        # pylint: disable-msg=E1101
+        # pylint misses AbstractCardSet methods
         oAC = AbstractCardSet.byName(self.view.sSetName)
 
-        oDlg = CreateCardSetDialog(self.parent,"PhysicalCardSet",oAC.author,oAC.comment)
+        oDlg = CreateCardSetDialog(self.parent, "PhysicalCardSet", oAC.author,
+                oAC.comment)
         oImport = gtk.CheckButton("Add cards to collection.")
+        # pylint: disable-msg=E1101
+        # pylint misses vbox methods
         oDlg.vbox.pack_start(oImport)
         oDlg.show_all()
         oDlg.run()
+        # pylint: enable-msg=E1101
 
         (sName, sAuthor, sDesc) = oDlg.get_data()
         if sName is None:
@@ -55,26 +72,32 @@ class PhysicalCardSetFromAbstract(CardListPlugin):
             do_complaint_error("Chosen Physical Card Set already exists")
             return
 
-        nP = PhysicalCardSet(name=sName)
-        nP.author = sAuthor
-        nP.comment = sDesc
-        nP.syncUpdate()
+        oNewPCS = PhysicalCardSet(name=sName)
+        oNewPCS.author = sAuthor
+        oNewPCS.comment = sDesc
+        oNewPCS.syncUpdate()
 
         # Add cards to physical card collection if requested
         if oImport.get_active():
             for oCard in self.model.getCardIterator(None):
+                # pylint: disable-msg=E1101
+                # pylint misses IAbstractCard methods
                 oAC = IAbstractCard(oCard)
-                PhysicalCard(abstractCard=oAC,expansion=None)
+                PhysicalCard(abstractCard=oAC, expansion=None)
             self.reload_all()
 
         # Populate the new physical card set
         aMissingCards = []
         for oCard in self.model.getCardIterator(None):
             oACard = IAbstractCard(oCard)
+            # pylint: disable-msg=E1101
+            # pylint misses AbstractCard methods (id)
             oPhysCards = PhysicalCard.selectBy(abstractCardID=oACard.id)
             for oPC in oPhysCards:
-                if oPC not in nP.cards:
-                    nP.addPhysicalCard(oPC.id)
+                if oPC not in oNewPCS.cards:
+                    # pylint: disable-msg=E1101
+                    # pylint misses PhysicalCardSet methods
+                    oNewPCS.addPhysicalCard(oPC.id)
                     break
             else:
                 aMissingCards.append(oACard)
@@ -82,10 +105,10 @@ class PhysicalCardSetFromAbstract(CardListPlugin):
         self.open_pcs(sName)
 
         if aMissingCards:
-            sMsg = "The following cards were not added to the physical card set " \
-                "because they are not present in your card collection:\n\n" \
-                + "\n".join([oC.name for oC in aMissingCards])
-            do_complaint_error(sMsg)
+            do_complaint_error("The following cards were not added to the" 
+                    " physical card set because they are not present in your" 
+                    " card collection: %s\n\n" % 
+                    "\n".join([oC.name for oC in aMissingCards]))
 
 # pylint: disable-msg=C0103
 # accept plugin name
