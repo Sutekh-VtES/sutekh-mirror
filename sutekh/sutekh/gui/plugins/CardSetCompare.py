9fe85242db657ab90c72337e67536ffd608c172f
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-03-21 21:56:28 +0000

    Redo Card Set Compare plugin to use notebook for display

--- sutekh/sutekh/gui/plugins/CardSetCompare.py
+++ sutekh/sutekh/gui/plugins/CardSetCompare.py
@@ -1,20 +1,24 @@
 # CardSetCompare.py
 # -*- coding: utf8 -*-
 # vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
-# Copyright 2006 Simon Cross <hodgestar@gmail.com>, Neil Muller <drnlmuller+sutekh@gmail.com>
+# Copyright 2006 Simon Cross <hodgestar@gmail.com>,
+# Copyright 2006, 2008 Neil Muller <drnlmuller+sutekh@gmail.com>
 # GPL - see COPYING for details
+"Compare the contents of two card sets"
 
 import gtk
 from sutekh.core.SutekhObjects import PhysicalCard, AbstractCardSet, \
-                                 PhysicalCardSet, AbstractCard, IAbstractCard
+        PhysicalCardSet, AbstractCard, IAbstractCard
 from sutekh.core.Filters import PhysicalCardSetFilter, AbstractCardSetFilter
 from sutekh.gui.PluginManager import CardListPlugin
 from sutekh.gui.SutekhDialog import SutekhDialog
 from sutekh.gui.ScrolledList import ScrolledList
+from sutekh.gui.AutoScrolledWindow import AutoScrolledWindow
 
 class CardSetCompare(CardListPlugin):
-    dTableVersions = {AbstractCardSet : [1, 2, 3],
-                      PhysicalCardSet : [1, 2, 3]}
+    "Compare to Card Sets"
+    dTableVersions = {AbstractCardSet : [3],
+                      PhysicalCardSet : [4]}
     aModelsSupported = [AbstractCardSet,
             PhysicalCardSet]
 
@@ -29,111 +33,128 @@ class CardSetCompare(CardListPlugin):
         return iDF
 
     def get_desired_menu(self):
+        "Register on the plugins menu"
         return "Plugins"
 
+    # pylint: disable-msg=W0613
+    # oWidget required by gtk function signature
     def activate(self, oWidget):
-        oDlg = self.make_dialog()
-        oDlg.run()
-        # only do stuff for AbstractCardSets
-
-    def make_dialog(self):
-        """
-        Create the list of card sets to select
-        """
-        self.oDlg = SutekhDialog("Choose Card Set to Compare with", self.parent,
+        "Handle menu activation"
+        oDlg = SutekhDialog("Choose Card Set to Compare with", self.parent,
                 gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                 (gtk.STOCK_OK, gtk.RESPONSE_OK,
                     gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
         if self._cModelType is AbstractCardSet:
             oSelect = AbstractCardSet.select().orderBy('name')
-            self.oCSList = ScrolledList('Abstract Card Sets')
+            oCSList = ScrolledList('Abstract Card Sets')
         elif self._cModelType is PhysicalCardSet:
             oSelect = PhysicalCardSet.select().orderBy('name')
-            self.oCSList = ScrolledList('Physical Card Sets')
+            oCSList = ScrolledList('Physical Card Sets')
         else:
             return
-        self.oCSList.set_select_single()
-        self.oDlg.vbox.pack_start(self.oCSList)
-        self.oCSList.set_size_request(150, 300)
+        oCSList.set_select_single()
+        # pylint: disable-msg=E1101
+        # pylint misses vbox methods
+        oDlg.vbox.pack_start(oCSList)
+        oCSList.set_size_request(150, 300)
         aVals = [oCS.name for oCS in oSelect if oCS.name != self.view.sSetName]
-        self.oCSList.fill_list(aVals)
-        self.oDlg.connect("response", self.handle_response)
-        self.oDlg.show_all()
-        return self.oDlg
+        oCSList.fill_list(aVals)
+        oDlg.connect("response", self.handle_response, oCSList)
+        oDlg.show_all()
+        oDlg.run()
+        oDlg.destroy()
 
-    def handle_response(self, oWidget, oResponse):
-        if oResponse ==  gtk.RESPONSE_OK:
+    def handle_response(self, oWidget, iResponse, oCSList):
+        "Handle response from the dialog"
+        if iResponse ==  gtk.RESPONSE_OK:
             aCardSetNames = [self.view.sSetName]
-            aCardSetNames.extend(self.oCSList.get_selection())
+            aCardSetNames.extend(oCSList.get_selection())
             self.comp_card_sets(aCardSetNames)
-        self.oDlg.destroy()
 
     def comp_card_sets(self, aCardSetNames):
+        "Display the results of comparing the card sets"
+        def format_list(aList, sColor):
+            "Format the list of cards for display"
+            oLabel = gtk.Label()
+            oAlign = gtk.Alignment()
+            oAlign.add(oLabel)
+            oAlign.set_padding(0, 0, 5, 0) # offset a little from the left edge
+            if len(aList) > 0:
+                sContents = ""
+                aList.sort()
+                aList.sort(key=lambda x: x[1], reverse=True)
+                # sort alphabetically, then reverse sort by card count
+                # stable sorting means this gives the desired ordering
+                for sCardName, iCount in aList:
+                    sContents += '%(num)d X <span foreground = "%(color)s">' \
+                            '%(name)s</span>\n' % {
+                                    'num' : iCount,
+                                    'color' : sColor,
+                                    'name' : sCardName,
+                                    }
+                oLabel.set_markup(sContents)
+            else:
+                oLabel.set_text('No Cards')
+            return oAlign
+
         (dDifferences, aCommon) = self.__get_card_set_list(aCardSetNames)
-        Results = SutekhDialog("Card Comparison", self.parent, gtk.DIALOG_MODAL | \
-                gtk.DIALOG_DESTROY_WITH_PARENT, \
+        oResultDlg = SutekhDialog("Card Comparison", self.parent,
+                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                 (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
-        myHBox = gtk.HBox(False, 0)
-        if len(aCommon) > 0:
-            oFrame = gtk.Frame("placeholder")
-            oFrame.get_label_widget().set_markup("<span foreground = \"blue\">Common Cards</span>")
-            message = ""
-            for sCardName, iCount in aCommon:
-                message += "<span foreground = \"green\">" + sCardName + \
-                        "</span> : " + str(iCount) + "\n"
-            myLabel = gtk.Label()
-            myLabel.set_markup(message)
-            oFrame.add(myLabel)
-            myHBox.pack_start(oFrame)
-        if len(dDifferences[aCardSetNames[0]]) > 0:
-            oFrame = gtk.Frame("placeholder")
-            oFrame.get_label_widget().set_markup("<span foreground = \"red\">Cards only in " + aCardSetNames[0] + "</span>")
-            message = ""
-            for sCardName, iCount in dDifferences[aCardSetNames[0]]:
-                message += "<span foreground = \"blue\">" + sCardName + "</span> : " + str(iCount) + "\n"
-            myLabel = gtk.Label()
-            myLabel.set_markup(message)
-            oFrame.add(myLabel)
-            myHBox.pack_start(oFrame)
-        if len(dDifferences[aCardSetNames[1]]) > 0:
-            oFrame = gtk.Frame("placeholder")
-            oFrame.get_label_widget().set_markup("<span foreground = \"red\">Cards only in " + aCardSetNames[1] + "</span>")
-            message = ""
-            for sCardName, iCount in dDifferences[aCardSetNames[1]]:
-                message += "<span foreground = \"blue\">" + sCardName + "</span> : " + str(iCount) + "\n"
-            myLabel = gtk.Label()
-            myLabel.set_markup(message)
-            oFrame.add(myLabel)
-            myHBox.pack_start(oFrame)
-        Results.vbox.pack_start(myHBox)
-        Results.show_all()
-        Results.run()
-        Results.destroy()
+        oNotebook = gtk.Notebook()
+        oNotebook.set_scrollable(True)
+        oNotebook.popup_enable()
+        oHeading = gtk.Label()
+        oHeading.set_markup('<span foreground = "blue">Common Cards</span>')
+        oComm = format_list(aCommon, 'green')
+        oNotebook.append_page(AutoScrolledWindow(oComm, True), oHeading)
+        oDiff1 = format_list(dDifferences[aCardSetNames[0]], 'red')
+        oHeading = gtk.Label()
+        oHeading.set_markup('<span foreground = "red">Cards only in %s</span>'
+                % aCardSetNames[0])
+        oNotebook.append_page(AutoScrolledWindow(oDiff1, True), oHeading)
+        oDiff2 = format_list(dDifferences[aCardSetNames[1]], 'red')
+        oHeading = gtk.Label()
+        oHeading.set_markup('<span foreground = "red">Cards only in %s</span>'
+                % aCardSetNames[1])
+        oNotebook.append_page(AutoScrolledWindow(oDiff2, True), oHeading)
+        # pylint: disable-msg=E1101
+        # pylint misses vbox methods
+        oResultDlg.vbox.pack_start(oNotebook)
+        oResultDlg.set_size_request(600, 600)
+        oResultDlg.show_all()
+        oResultDlg.run()
+        oResultDlg.destroy()
 
     def __get_card_set_list(self, aCardSetNames):
+        "Get the differences between the card sets"
+        # Only compare abstract cards
         dFullCardList = {}
-        sCardSetName1 = aCardSetNames[0]
-        sCardSetName2 = aCardSetNames[1]
         for sCardSetName in aCardSetNames:
             if self._cModelType is AbstractCardSet:
                 oFilter = AbstractCardSetFilter(sCardSetName)
-                oCS = oFilter.select(AbstractCard)
+                oCardSet = oFilter.select(AbstractCard)
             elif self._cModelType is PhysicalCardSet:
                 oFilter = PhysicalCardSetFilter(sCardSetName)
-                oCS = oFilter.select(PhysicalCard)
-            for oC in oCS:
-                oAC = IAbstractCard(oC)
-                dFullCardList.setdefault(oAC.name, {sCardSetName1 : 0, sCardSetName2 : 0})
-                dFullCardList[oAC.name][sCardSetName] += 1
-        dDifferences = { sCardSetName1 : [], sCardSetName2 : [] }
+                oCardSet = oFilter.select(PhysicalCard)
+            for oCard in oCardSet:
+                # pylint: disable-msg=E1101
+                # pylint doesn't see IAbstractCard methods
+                oAbsCard = IAbstractCard(oCard)
+                dFullCardList.setdefault(oAbsCard.name, {aCardSetNames[0] : 0,
+                    aCardSetNames[1] : 0})
+                dFullCardList[oAbsCard.name][sCardSetName] += 1
+        dDifferences = { aCardSetNames[0] : [], aCardSetNames[1] : [] }
         aCommon = []
-        for sCardName in dFullCardList.keys():
-            iDiff = dFullCardList[sCardName][sCardSetName1] - dFullCardList[sCardName][sCardSetName2]
-            iCommon = min(dFullCardList[sCardName][sCardSetName1], dFullCardList[sCardName][sCardSetName2])
+        for sCardName in dFullCardList:
+            iDiff = dFullCardList[sCardName][aCardSetNames[0]] - \
+                    dFullCardList[sCardName][aCardSetNames[1]]
+            iCommon = min(dFullCardList[sCardName][aCardSetNames[0]],
+                    dFullCardList[sCardName][aCardSetNames[1]])
             if iDiff > 0:
-                dDifferences[sCardSetName1].append( (sCardName, iDiff) )
+                dDifferences[aCardSetNames[0]].append( (sCardName, iDiff) )
             elif iDiff < 0:
-                dDifferences[sCardSetName2].append( (sCardName, abs(iDiff)) )
+                dDifferences[aCardSetNames[1]].append( (sCardName, abs(iDiff)) )
             if iCommon > 0:
                 aCommon.append((sCardName, iCommon))
         return (dDifferences, aCommon)
