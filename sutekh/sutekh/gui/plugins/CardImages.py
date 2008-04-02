11ac7a2ff5ea92418ba5afd79d980f4a7af3810f
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-04-02 16:52:02 +0000

    Scale images to pane by default - allow setting different modes va context menu

--- sutekh/sutekh/gui/plugins/CardImages.py
+++ sutekh/sutekh/gui/plugins/CardImages.py
@@ -25,7 +25,27 @@ from sutekh.gui.SutekhDialog import SutekhDialog, do_complaint_buttons, \
 from sutekh.gui.AutoScrolledWindow import AutoScrolledWindow
 from sutekh.SutekhUtility import prefs_dir, ensure_dir_exists
 
-def check_file(sFileName):
+
+_iFORWARD, _iBACKWARD = range(2)
+_iFULL, _iVIEW_FIXED, _iFIT = range(3)
+_tRATIO = (225, 300)
+
+def _scale_dims(iImageWidth, iImageHeight, iPaneWidth, iPaneHeight):
+    """Rescale the image dimension so they fit in the pane, preserving the
+       aspect ratiom."""
+    fImageAspectRatio = float(iImageHeight) / float(iImageWidth)
+    fPaneAspectRatio = float(iPaneHeight) / float(iPaneWidth)
+
+    if fPaneAspectRatio > fImageAspectRatio:
+        # wider
+        fDestWidth = iPaneWidth
+        fDestHeight = iPaneWidth * fImageAspectRatio
+    elif fPaneAspectRatio < fImageAspectRatio:
+        fDestHeight = iPaneHeight
+        fDestWidth = iPaneHeight / fImageAspectRatio
+    return int(fDestWidth), int(fDestHeight)
+
+def _check_file(sFileName):
     """Check if file exists and is readable"""
     bRes = True
     try:
@@ -35,7 +55,7 @@ def check_file(sFileName):
         bRes = False
     return bRes
 
-def unaccent(sCardName):
+def _unaccent(sCardName):
     """
     Remove unicode accents
     """
@@ -45,6 +65,64 @@ def unaccent(sCardName):
             unicode(sCardName.encode('utf8'), encoding='utf-8'))
     # Drop non-ascii characters
     return "".join(b for b in sNormed.encode('utf8') if ord(b) < 128)
+
+class CardImagePopupMenu(gtk.Menu):
+    # pylint: disable-msg=R0904
+    # R0904 - can't not trigger these warning with pygtk
+    """Popup menu for the Card Image Frame"""
+
+    def __init__(self, oFrame, iZoomMode):
+        super(CardImagePopupMenu, self).__init__()
+        self.oFrame = oFrame
+        self.oZoom = gtk.RadioAction('Zoom', 'Show images at original size',
+                None, None, _iFULL)
+        self.oZoom.set_group(None)
+        self.oViewFixed = gtk.RadioAction('ViewFixed',
+                'Show images at fixed size', None, None, _iVIEW_FIXED)
+        self.oViewFixed.set_group(self.oZoom)
+        self.oViewFit = gtk.RadioAction('ViewFit', 'Fit images to the pane',
+                None, None, _iFIT)
+        self.oViewFit.set_group(self.oZoom)
+        self.oNext = gtk.Action('NextExp', 'Show next expansion image', None,
+                None)
+        self.oPrev = gtk.Action('PrevExp', 'Show previous expansion image',
+                None, None)
+
+        self.oPrev.connect('activate', self.cycle_expansion, _iBACKWARD)
+        self.oNext.connect('activate', self.cycle_expansion, _iFORWARD)
+        self.oViewFit.connect('activate', self.set_zoom, _iFIT)
+        self.oZoom.connect('activate', self.set_zoom, _iFULL)
+        self.oViewFixed.connect('activate', self.set_zoom, _iVIEW_FIXED)
+
+        if iZoomMode == _iFULL:
+            self.oZoom.set_active(True)
+        elif iZoomMode == _iVIEW_FIXED:
+            self.oViewFixed.set_active(True)
+        elif iZoomMode == _iFIT:
+            self.oViewFit.set_active(True)
+
+        self.add(self.oViewFit.create_menu_item())
+        self.add(self.oViewFixed.create_menu_item())
+        self.add(self.oZoom.create_menu_item())
+        self.add(self.oNext.create_menu_item())
+        self.add(self.oPrev.create_menu_item())
+
+    def set_show_expansion_state(self, bValue):
+        """Grey out the expansion menus if needed"""
+        self.oNext.set_sensitive(bValue)
+        self.oPrev.set_sensitive(bValue)
+
+    # pylint: disable-msg=W0613
+    # oWidget needed by gtk function signature
+    def cycle_expansion(self, oWidget, iDir):
+        """Change the expansion as requested."""
+        assert(iDir in [_iBACKWARD, _iFORWARD])
+        self.oFrame.do_cycle_expansion(iDir)
+
+    def set_zoom(self, oWidget, iScale):
+        """Change the drawing mode."""
+        assert(iScale in [_iFULL, _iVIEW_FIXED, _iFIT])
+        self.oFrame.set_zoom_mode(iScale)
 
 class CardImageFrame(BasicFrame, CardListViewListener):
     # pylint: disable-msg=R0901, R0904, R0902
@@ -65,6 +143,8 @@ class CardImageFrame(BasicFrame, CardListViewListener):
         oVBox.pack_start(self.oExpansionLabel, False, False)
         oVBox.pack_start(oBox)
         self._oView = AutoScrolledWindow(oVBox, True)
+        self._oView.get_hadjustment().connect('changed', self._pane_adjust)
+        self._oView.get_vadjustment().connect('changed', self._pane_adjust)
         self._oImage = gtk.Image()
         self._oImage.set_from_stock(gtk.STOCK_MISSING_IMAGE,
                 gtk.ICON_SIZE_DIALOG)
@@ -80,8 +160,6 @@ class CardImageFrame(BasicFrame, CardListViewListener):
         oBox.connect('drag-motion', self.drag_motion)
         oBox.connect('button-press-event', self.__cycle_expansion)
 
-        self._oImage.set_alignment(0.5, 0.5) # Centre image
-
         self.__sPrefsPath = self._oConfigFile.get_plugin_key('card image path')
         if self.__sPrefsPath is None:
             self.__sPrefsPath = os.path.join(prefs_dir('Sutekh'), 'cardimages')
@@ -92,6 +170,8 @@ class CardImageFrame(BasicFrame, CardListViewListener):
         self.__aExpansions = []
         self.__iExpansionPos = 0
         self.__sCardName = ''
+        self.__iZoomMode = _iFIT
+        self.tOldSize = (0, 0)
 
     type = property(fget=lambda self: "Card Image Frame", doc="Frame Type")
 
@@ -103,7 +183,7 @@ class CardImageFrame(BasicFrame, CardListViewListener):
             sTestFile = os.path.join(self.__sPrefsPath, 'bh', 'acrobatics.jpg')
         else:
             sTestFile = os.path.join(sTestPath, 'bh', 'acrobatics.jpg')
-        return check_file(sTestFile)
+        return _check_file(sTestFile)
 
     def __convert_expansion(self, sExpansionName):
         """Convert the Full Expansion name into the abbrevation needed."""
@@ -133,12 +213,20 @@ class CardImageFrame(BasicFrame, CardListViewListener):
             self.__aExpansions = []
             self.__iExpansionPos = 0
 
+    def __redraw(self):
+        """Redraw the current card"""
+        # If further events are pending, don't try and redraw
+        if gtk.gdk.events_pending():
+            return
+        sFullFilename = self.__convert_cardname()
+        self.__load_image(sFullFilename)
+
     def __convert_cardname(self):
         """
         Convert sCardName to the form used by the card image list
         """
         sCurExpansionPath = self.__convert_expansion(self.__sCurExpansion)
-        sFilename = unaccent(self.__sCardName.lower())
+        sFilename = _unaccent(self.__sCardName.lower())
         if sFilename.find('the ') == 0:
             sFilename = sFilename[4:] + 'the'
         sFilename = sFilename.replace('(advanced)', 'adv')
@@ -150,36 +238,41 @@ class CardImageFrame(BasicFrame, CardListViewListener):
 
     def __load_image(self, sFullFilename):
         "Load an image into the pane, show broken image if needed"
+        self._oImage.set_alignment(0.5, 0.5) # Centre image
         try:
             if self.__bShowExpansions:
                 self.oExpansionLabel.set_markup('<i>Image from expansion :'
                         ' </i> %s' % self.__sCurExpansion)
                 self.oExpansionLabel.show()
+                # pylint: disable-msg=E1101
+                # pylint doesn't pick up allocation methods correctly
+                iHeightOffset = self.oExpansionLabel.allocation.height + 2
             else:
                 self.oExpansionLabel.hide() # config chanes can cause this
+                iHeightOffset = 0
             oPixbuf = gtk.gdk.pixbuf_new_from_file(sFullFilename)
             iWidth = oPixbuf.get_width()
             iHeight = oPixbuf.get_height()
-            # We scale images larger than 300 wide or 410 high
-            if iWidth > 300 or iHeight > 410:
-                # We want to scale image to match 300x400 (because the
-                # igure neater than 300/410) as closely as
-                # possible without changing the aspect ratio
-                fAspectRatio = float(iWidth)/float(iHeight)
-                if fAspectRatio > 0.75:
-                    # Wider aspect than 300x400
-                    iDestWidth = 300
-                    iDestHeight = int(300/fAspectRatio)
-                elif fAspectRatio < 0.75:
-                    iDestHeight = 400
-                    iDestWidth = int(400*fAspectRatio)
-                else:
-                    iDestHeight = 400
-                    iDestWidth = 300
+            if self.__iZoomMode == _iFIT:
+                # Need to fix aspect ratios
+                iPaneHeight = self._oView.get_vadjustment().page_size - \
+                        iHeightOffset
+                iPaneWidth = self._oView.get_hadjustment().page_size
+                # don't centre image under label
+                self._oImage.set_alignment(0, 0.5)
+                iDestWidth, iDestHeight = _scale_dims(iWidth, iHeight,
+                        iPaneWidth, iPaneHeight)
+                self._oImage.set_from_pixbuf(oPixbuf.scale_simple(iDestWidth,
+                    iDestHeight, gtk.gdk.INTERP_HYPER))
+            elif self.__iZoomMode == _iVIEW_FIXED:
+                iDestWidth, iDestHeight = _scale_dims(iWidth, iHeight,
+                        _tRATIO[0], _tRATIO[1])
                 self._oImage.set_from_pixbuf(oPixbuf.scale_simple(iDestWidth,
                     iDestHeight, gtk.gdk.INTERP_HYPER))
             else:
+                # Full size, so no scaling
                 self._oImage.set_from_pixbuf(oPixbuf)
+
         except gobject.GError:
             self._oImage.set_from_stock(gtk.STOCK_MISSING_IMAGE,
                     gtk.ICON_SIZE_DIALOG)
@@ -194,7 +287,7 @@ class CardImageFrame(BasicFrame, CardListViewListener):
             sTestFile = os.path.join(self.__sPrefsPath, 'acrobatics.jpg')
         else:
             sTestFile = os.path.join(sTestPath, 'acrobatics.jpg')
-        return check_file(sTestFile)
+        return _check_file(sTestFile)
 
     def update_config_path(self, sNewPath):
         """Update the path we use to search for expansions."""
@@ -217,36 +310,49 @@ class CardImageFrame(BasicFrame, CardListViewListener):
                 # Set self.__sCurExpansion to a valid value
                 self.__sCurExpansion = self.__aExpansions[0]
                 self.__iExpansionPos = 0
-        sFullFilename = self.__convert_cardname()
-        self.__load_image(sFullFilename)
+        self.__redraw()
 
+    def do_cycle_expansion(self, iDir):
+        """Change the expansion image to a different one in the list."""
+        if len(self.__aExpansions) < 2 or not self.__bShowExpansions:
+            return # nothing to scroll through
+        if iDir == _iFORWARD:
+            self.__iExpansionPos += 1
+            if self.__iExpansionPos >= len(self.__aExpansions):
+                self.__iExpansionPos = 0
+        elif iDir == _iBACKWARD:
+            self.__iExpansionPos -= 1
+            if self.__iExpansionPos < 0:
+                self.__iExpansionPos = len(self.__aExpansions) - 1
+        self.__sCurExpansion = self.__aExpansions[self.__iExpansionPos]
+        self.__redraw()
+
+    def set_zoom_mode(self, iScale):
+        """Update the zoom mode."""
+        self.__iZoomMode = iScale
+        self.__redraw()
 
     # pylint: disable-msg=W0613
     # oWidget needed by gtk function signature
     def __cycle_expansion(self, oWidget, oEvent):
         """On a button click, move to the next expansion."""
-        bRedraw = False
-        if len(self.__aExpansions) < 2:
-            return True # nothing to scroll through
         if oEvent.type != gtk.gdk.BUTTON_PRESS:
             return True # don't jump twice on double or triple clicks
         if oEvent.button == 1:
-            # left button, go forward
-            self.__iExpansionPos += 1
-            if self.__iExpansionPos >= len(self.__aExpansions):
-                self.__iExpansionPos = 0
-            bRedraw = True
+            self.do_cycle_expansion(_iFORWARD)
         elif oEvent.button == 3:
-            # Right button, go backwards
-            self.__iExpansionPos -= 1
-            if self.__iExpansionPos < 0:
-                self.__iExpansionPos += len(self.__aExpansions)
-            bRedraw = True
-        if bRedraw:
-            self.__sCurExpansion = self.__aExpansions[self.__iExpansionPos]
-            sFullFilename = self.__convert_cardname()
-            self.__load_image(sFullFilename)
+            # Do context menu
+            oPopupMenu = CardImagePopupMenu(self, self.__iZoomMode)
+            oPopupMenu.set_show_expansion_state(self.__bShowExpansions and
+                    len(self.__aExpansions) > 1)
+            oPopupMenu.popup(None, None, None, oEvent.button, oEvent.time)
         return True
+
+    # W0613 oAdjust also required by signature
+    def _pane_adjust(self, oAdjust):
+        """Redraw the image if needed when the pane size changes."""
+        if self.__iZoomMode == _iFIT:
+            self.__redraw()
 
 # pylint: disable-msg=R0904
 # R0904 - gtk Widget, so has many public methods
@@ -466,7 +572,7 @@ class CardImagePlugin(CardListPlugin):
 
     def _unzip_file(self, sFileName):
         """Unzip a file containing the images."""
-        if check_file(sFileName):
+        if _check_file(sFileName):
             try:
                 oZipFile = zipfile.ZipFile(sFileName, 'r')
             except zipfile.BadZipfile:
