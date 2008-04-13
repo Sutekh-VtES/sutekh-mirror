f95d544ea31a8cf7def1421b4aff55890b260927
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-04-13 17:11:36 +0000

    Refactor card handling - split it smaller, hopefully more readable functions.

--- sutekh/sutekh/gui/PhysicalCardController.py
+++ sutekh/sutekh/gui/PhysicalCardController.py
@@ -15,6 +15,17 @@ from sutekh.gui.PhysicalCardView import PhysicalCardView
 from sutekh.core.SutekhObjects import PhysicalCard, AbstractCard, \
         IExpansion, MapPhysicalCardToPhysicalCardSet
 
+def _prompt_text(iCnt, aPCS):
+    """Format text to prompt for card deletion nicely."""
+    if iCnt > 1:
+        sText = "Card Present in the following %d Physical Card Sets:\n" % iCnt
+    else:
+        sText = "Card Present in the following Physical Card Set:\n"
+    for oPCS in aPCS:
+        sText += '<span foreground = "blue">%s</span>\n' % oPCS.name
+    sText += "<b>Really Delete?</b>"
+    return sText
+
 class PhysicalCardController(object):
     """Controller for the Physical Card Collection.
 
@@ -47,93 +58,9 @@ class PhysicalCardController(object):
             return False
 
         if sExpansion is None:
-            # Removing a card in the list
-            aCandCards = PhysicalCard.selectBy(abstractCardID=oAbsCard.id)
-            # check we found something?
-            if aCandCards.count() == 0:
-                raise RuntimeError("Model inconsistent with the database")
-            # Logic is:
-            # Prefer cards with no expansion set and in no PCS
-            # else cards in no PCS
-            # else card in the fewest PCS's
-            dPCS = {}
-            aCandsExpansion = []
-            for oPhysCard in aCandCards.reversed():
-                aCardSetMapping = list(
-                        MapPhysicalCardToPhysicalCardSet.selectBy(
-                            physicalCardID=oPhysCard.id))
-                iCount = len(aCardSetMapping)
-                if iCount == 0:
-                    if oPhysCard.expansion is None:
-                        # OK, can delete this one and be done with it
-                        PhysicalCard.delete(oPhysCard.id)
-                        self.model.dec_card_expansion_by_name(oAbsCard.name,
-                                None)
-                        self.model.dec_card_by_name(oAbsCard.name)
-                        send_reload_signal(oAbsCard)
-                        return True
-                    else:
-                        aCandsExpansion.append(oPhysCard)
-                dPCS[oPhysCard] = [iCount, [x.physicalCardSet for x in
-                    aCardSetMapping]]
-            if len(aCandsExpansion) > 0:
-                # ditto
-                oPhysCard = aCandsExpansion[-1]
-                sExpName = oPhysCard.expansion.name
-                PhysicalCard.delete(oPhysCard.id)
-                self.model.dec_card_expansion_by_name(oAbsCard.name, sExpName)
-                self.model.dec_card_by_name(oAbsCard.name)
-                send_reload_signal(oAbsCard)
-                return True
-            # All physical cards are assigned to PhysicalCardSets, so find the
-            # one in the fewest
-            aList = sorted(dPCS.items(), key=lambda x: x[1][0], reverse=True)
-            # Sort by count, take the last one as smallest
-            oPhysCard, (iCnt, aPCS) = aList[-1]
-            # This is probably overcomplicated, need to revisit this sometime
-            # Prompt the user for confirmation
-            if iCnt > 1:
-                sText = "Card Present in the following %d Physical Card" \
-                        " Sets:\n" % iCnt
-            else:
-                sText = "Card Present in the following Physical Card Set:\n"
-            for oPCS in aPCS:
-                sText += '<span foreground = "blue">%s</span>\n' % oPCS.name
-            sText += "<b>Really Delete?</b>"
-            iResponse = do_complaint(sText, gtk.MESSAGE_QUESTION,
-                    gtk.BUTTONS_YES_NO, True)
-            if iResponse == gtk.RESPONSE_YES:
-                # User agrees
-                # Delete card from all the PhysicalCardSets first
-                for oPCS in aPCS:
-                    oPCS.removePhysicalCard(oPhysCard.id)
-                oExpansion = oPhysCard.expansion
-                PhysicalCard.delete(oPhysCard.id)
-                if oExpansion is not None:
-                    self.model.dec_card_expansion_by_name(oAbsCard.name,
-                            oExpansion.name)
-                else:
-                    self.model.dec_card_expansion_by_name(oAbsCard.name, None)
-                self.model.dec_card_by_name(oAbsCard.name)
-                # SQLObject Events should take care of updating any open
-                # card sets
-                send_reload_signal(oAbsCard)
+            return self._remove_card_from_list(oAbsCard)
         else:
-            # We are fiddling between the expansions
-            oThisExpansion = IExpansion(sExpansion)
-            aPhysCards = list(PhysicalCard.selectBy(abstractCardID=oAbsCard.id,
-                expansionID=oThisExpansion.id))
-            if len(aPhysCards) < 1:
-                raise RuntimeError("Model inconsistent with the database")
-            oThisCard = aPhysCards[-1] # last card, general principles
-            # Update card
-            oThisCard.expansion = None
-            oThisCard.sync()
-            # Update model
-            self.model.dec_card_expansion_by_name(oAbsCard.name, sExpansion)
-            self.model.inc_card_expansion_by_name(oAbsCard.name, None)
-            send_reload_signal(oAbsCard)
-            return True
+            return self._remove_card_from_expansion(oAbsCard, sExpansion)
 
     def inc_card(self, sName, sExpansion):
         """Returns True if a card was successfully added, False otherwise."""
@@ -150,36 +77,148 @@ class PhysicalCardController(object):
 
         if sExpansion is None:
             # Adding a new card to the list
-            aExpansion = set([oRP.expansion for oRP in oAbsCard.rarity])
-            if len(aExpansion) == 1:
-                oExpansion = aExpansion.pop()
-                sExpansion = oExpansion.name
-            else:
-                oExpansion = None
-            # pylint: disable-msg=W0612
-            # Creating a new PhysicalCard in the database is all that is needed
-            oNewPC = PhysicalCard(abstractCard=oAbsCard, expansion=oExpansion)
-            self.model.inc_card_by_name(oAbsCard.name)
-            self.model.inc_card_expansion_by_name(oAbsCard.name, sExpansion)
-            send_reload_signal(oAbsCard)
+            return self._add_card_to_list(oAbsCard)
         else:
-            # We are fiddling between the expansions
-            # Find a card with no expansion
-            aPhysCards = list(PhysicalCard.selectBy(abstractCardID=oAbsCard.id,
-                expansionID=None))
-            if len(aPhysCards) < 1:
-                raise RuntimeError("Model inconsistent with the database")
-            oThisCard = aPhysCards[-1] # last card, general principles
-            # Update card
-            oThisCard.expansion = IExpansion(sExpansion)
-            oThisCard.sync()
-            # Update model
-            self.model.dec_card_expansion_by_name(oAbsCard.name, None)
-            self.model.inc_card_expansion_by_name(oAbsCard.name, sExpansion)
-            send_reload_signal(oAbsCard)
+            self._add_card_to_expansion(oAbsCard, sExpansion)
         return True
 
     def set_card_text(self, sCardName):
         """Set the card text to reflect the selected card."""
         self.__oMainWin.set_card_text(sCardName)
 
+    # help methods for managing the card counts
+
+    def _remove_card_from_list(self, oAbsCard):
+        """Remove a card entirely from the list.
+
+           We prefer cards with no expansion info set and in no PCS,
+           then cards in no PCS, then the card in the fewest PCS's.
+           """
+        # pylint: disable-msg=E1101
+        # SQLObject confuses pylint
+        aCandCards = PhysicalCard.selectBy(abstractCardID=oAbsCard.id)
+        # check we found something?
+        if aCandCards.count() == 0:
+            raise RuntimeError("Model inconsistent with the database")
+        # Logic is:
+        dPCS = {}
+        aCandsExpansion = []
+        # we reverse the list, so we look at newest cards first.
+        for oPhysCard in aCandCards.reversed():
+            aCardSetMapping = MapPhysicalCardToPhysicalCardSet.selectBy(
+                physicalCardID=oPhysCard.id)
+            iCount = aCardSetMapping.count()
+            if iCount == 0:
+                if oPhysCard.expansion is None:
+                    # OK, can delete this one and be done with it
+                    self._delete_card(oPhysCard, oAbsCard)
+                    return True
+                else:
+                    aCandsExpansion.append(oPhysCard)
+            dPCS[oPhysCard] = [iCount, [x.physicalCardSet for x in
+                aCardSetMapping]]
+        if len(aCandsExpansion) > 0:
+            # OK, can delete this one
+            # Take the first candidate, which should be the most recent
+            oPhysCard = aCandsExpansion[0]
+            self._delete_card(oPhysCard, oAbsCard)
+            return True
+        # All physical cards are assigned to PhysicalCardSets, so find the
+        # one in the fewest
+        # This is probably overcomplicated, need to revisit this sometime
+        # Sort by count, take the smallest
+        oPhysCard, (iCnt, aPCS) = sorted(dPCS.items(),
+                key=lambda x: x[1][0])[0]
+        # Prompt the user for confirmation
+        if do_complaint(_prompt_text(iCnt, aPCS), gtk.MESSAGE_QUESTION,
+                gtk.BUTTONS_YES_NO, True) == gtk.RESPONSE_YES:
+            # User agrees
+            # Delete card from all the PhysicalCardSets first
+            for oPCS in aPCS:
+                oPCS.removePhysicalCard(oPhysCard.id)
+            self._delete_card(oPhysCard, oAbsCard)
+            return True
+        else:
+            # user disagrees
+            return False
+
+    def _add_card_to_list(self, oAbsCard):
+        """Add a card to the list.
+
+           Set the expansion if there is only 1 candidate.
+           """
+        # pylint: disable-msg=E1101
+        # SQLObject confuses pylint
+        aExpansion = set([oRP.expansion for oRP in oAbsCard.rarity])
+        if len(aExpansion) == 1:
+            oExpansion = aExpansion.pop()
+            sExpansion = oExpansion.name
+        else:
+            oExpansion = None
+        # pylint: disable-msg=W0612
+        # Creating a new PhysicalCard in the database is all that is needed
+        oNewPC = PhysicalCard(abstractCard=oAbsCard, expansion=oExpansion)
+        self.model.inc_card_by_name(oAbsCard.name)
+        self.model.inc_card_expansion_by_name(oAbsCard.name, sExpansion)
+        send_reload_signal(oAbsCard)
+        return True
+
+    def _remove_card_from_expansion(self, oAbsCard, sExpansion):
+        """Move card from this expansion to no expansion.
+
+           Decrement the count for the card with expansion, and
+           increment the count for cards with no expansions."""
+        # pylint: disable-msg=E1101
+        # SQLObject confuses pylint
+        oThisExpansion = IExpansion(sExpansion)
+        aPhysCards = list(PhysicalCard.selectBy(abstractCardID=oAbsCard.id,
+            expansionID=oThisExpansion.id))
+        if len(aPhysCards) < 1:
+            raise RuntimeError("Model inconsistent with the database")
+        oThisCard = aPhysCards[-1] # last card, general principles
+        # Update card
+        oThisCard.expansion = None
+        oThisCard.sync()
+        # Update model
+        self.model.dec_card_expansion_by_name(oAbsCard.name, sExpansion)
+        self.model.inc_card_expansion_by_name(oAbsCard.name, None)
+        send_reload_signal(oAbsCard)
+        return True
+
+    def _add_card_to_expansion(self, oAbsCard, sExpansion):
+        """Move a card from no exppansion to this expansion.
+
+           Increase the count of the card with the given expansion and
+           decrease the count of the cards with no expansion.
+           """
+        # pylint: disable-msg=E1101
+        # SQLObject confuses pylint
+        # Find a card with no expansion
+        aPhysCards = list(PhysicalCard.selectBy(abstractCardID=oAbsCard.id,
+            expansionID=None))
+        if len(aPhysCards) < 1:
+            raise RuntimeError("Model inconsistent with the database")
+        oThisCard = aPhysCards[-1] # last card, general principles
+        # Update card
+        oThisCard.expansion = IExpansion(sExpansion)
+        oThisCard.sync()
+        # Update model
+        self.model.dec_card_expansion_by_name(oAbsCard.name, None)
+        self.model.inc_card_expansion_by_name(oAbsCard.name, sExpansion)
+        send_reload_signal(oAbsCard)
+
+    def _delete_card(self, oPhysCard, oAbsCard):
+        """Delete a card from the PhysicalCardSet and update the model."""
+        # pylint: disable-msg=E1101
+        # SQLObject confuses pylint
+        oExpansion = oPhysCard.expansion
+        PhysicalCard.delete(oPhysCard.id)
+        if oExpansion:
+            self.model.dec_card_expansion_by_name(oAbsCard.name,
+                    oExpansion.name)
+        else:
+            self.model.dec_card_expansion_by_name(oAbsCard.name, None)
+        self.model.dec_card_by_name(oAbsCard.name)
+        # SQLObject Events should take care of updating any open
+        # card sets
+        send_reload_signal(oAbsCard)
