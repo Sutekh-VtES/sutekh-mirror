d5eea43f84c18ca16d4fdd4af49e7ab1dc5a58e6
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-04-13 07:39:14 +0000

    Run tests using nosetests if available

--- tools/run_all_tests.sh
+++ tools/run_all_tests.sh
@@ -3,8 +3,14 @@
 # Arguably not copyrightable, since this is basically self-evident,
 # but, if a license is needed, consider this to be in the public domain
 
-export PYTHONPATH=../sutekh
-for file in ../sutekh/sutekh/tests/test_*py; do
-   echo "Running $file"
-   python $file
-done
+if which nosetests > /dev/null; then
+   # Use nosetests if available
+   echo "Running tests using nosetests"
+   (cd ../sutekh && nosetests sutekh/tests)
+else
+   export PYTHONPATH=../sutekh
+   for file in ../sutekh/sutekh/tests/test_*py; do
+      echo "Running $file"
+      python $file
+   done
+fi
