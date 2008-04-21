c7163e1d4cb4fe82d897291ff3d0b57a797fa7a0
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-04-21 14:33:31 +0000

    Pylint cleanups

--- sutekh/sutekh/gui/plugins/CSVImporter.py
+++ sutekh/sutekh/gui/plugins/CSVImporter.py
@@ -8,7 +8,8 @@ import gtk
 import csv
 import gobject
 from sutekh.io.CSVParser import CSVParser
-from sutekh.core.SutekhObjects import PhysicalCard, AbstractCardSet, PhysicalCardSet
+from sutekh.core.SutekhObjects import PhysicalCard, AbstractCardSet, \
+        PhysicalCardSet
 from sutekh.core.CardLookup import LookupFailed
 from sutekh.gui.PluginManager import CardListPlugin
 from sutekh.gui.SutekhDialog import SutekhDialog, do_complaint_error
@@ -22,8 +23,10 @@ class CSVImporter(CardListPlugin):
     }
     aModelsSupported = [AbstractCardSet, PhysicalCardSet, PhysicalCard]
 
-    def __init__(self,*args,**kws):
-        super(CSVImporter,self).__init__(*args,**kws)
+    # pylint: disable-msg=W0142
+    # **magic OK here
+    def __init__(self, *aArgs, **kwargs):
+        super(CSVImporter, self).__init__(*aArgs, **kwargs)
         self._dCSVImportDestinations = {
             'Physical Cards': (CSVParser.PCL, None),
             'Physical Card Set': (CSVParser.PCS, PhysicalCardSet),
@@ -31,34 +34,43 @@ class CSVImporter(CardListPlugin):
         }
 
     def get_menu_item(self):
-        """
-        Overrides method from base class.
-        """
+        """Overrides method from base class. Register on the 'Plugins' menu"""
         if not self.check_versions() or not self.check_model_type():
             return None
-        iDF = gtk.MenuItem("Import CSV File")
-        iDF.connect("activate", self.activate)
-        return iDF
+        oMenuItem = gtk.MenuItem("Import CSV File")
+        oMenuItem.connect("activate", self.activate)
+        return ('Plugins', oMenuItem)
 
-    def get_desired_menu(self):
-        return "Plugins"
-
-    def activate(self,oWidget):
+    # pylint: disable-msg=W0613
+    # oWidget required by function signature
+    def activate(self, oWidget):
+        """Handle response form the user"""
         oDlg = self.make_dialog()
         oDlg.run()
 
+    # pylint: enable-msg=W0613
+
+    # pylint: disable-msg=W0201
+    # defining elements outside of init is OK here, because of plugin structure
     def make_dialog(self):
-        self.oDlg = SutekhDialog("Choose CSV File",None,
+        """Create the dialog.
+
+           Allow the user to select a file and specify which columns to
+           use for card name, card count and expansions.
+           """
+        self.oDlg = SutekhDialog("Choose CSV File", None,
                 gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                 (gtk.STOCK_OK, gtk.RESPONSE_OK,
                  gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
 
         oLabel = gtk.Label()
         oLabel.set_markup("<b>Select CSV File ...</b>")
-        oLabel.set_alignment(0.0,0.5)
-        self.oDlg.vbox.pack_start(oLabel,padding=5)
+        oLabel.set_alignment(0.0, 0.5)
+        # pylint: disable-msg=E1101
+        # vbox confuses pylint
+        self.oDlg.vbox.pack_start(oLabel, padding=5)
 
-        self.oFileChooser = SutekhFileButton(self.parent,"Select a CSV File")
+        self.oFileChooser = SutekhFileButton(self.parent, "Select a CSV File")
         self.oFileChooser.connect("selection-changed",
                                   self._selected_file_changed)
         self.oDlg.vbox.pack_start(self.oFileChooser)
@@ -90,21 +102,21 @@ class CSVImporter(CardListPlugin):
 
         self._clear_column_selectors()
 
-        self.oDlg.vbox.pack_start(gtk.HSeparator(),padding=5)
+        self.oDlg.vbox.pack_start(gtk.HSeparator(), padding=5)
 
         oLabel = gtk.Label()
         oLabel.set_markup("<b>Import as ...</b>")
-        oLabel.set_alignment(0.0,0.5)
+        oLabel.set_alignment(0.0, 0.5)
         self.oDlg.vbox.pack_start(oLabel)
 
-        oIter = self._dCSVImportDestinations.iteritems()
-        for sName, (iType, cCardSet) in oIter:
-            self._oFirstBut = gtk.RadioButton(None,sName,False)
+        oIter = self._dCSVImportDestinations.iterkeys()
+        for sName in oIter:
+            self._oFirstBut = gtk.RadioButton(None, sName, False)
             self._oFirstBut.set_active(True)
             self.oDlg.vbox.pack_start(self._oFirstBut)
             break
-        for sName, iType in oIter:
-            oBut = gtk.RadioButton(self._oFirstBut,sName)
+        for sName in oIter:
+            oBut = gtk.RadioButton(self._oFirstBut, sName)
             self.oDlg.vbox.pack_start(oBut)
 
         oCardSetNameBox = gtk.HBox()
@@ -114,23 +126,26 @@ class CSVImporter(CardListPlugin):
         self.oDlg.vbox.pack_start(oCardSetNameBox)
 
         self.oDlg.connect("response", self.handle_response)
-        self.oDlg.set_size_request(400,400)
+        self.oDlg.set_size_request(400, 400)
         self.oDlg.show_all()
 
         return self.oDlg
 
     def _selected_file_changed(self, oFileChooser):
         sFile = oFileChooser.get_filename()
+
+        # pylint: disable-msg=W0703
+        # We want to catch all exceptions here
         try:
-            fIn = file(sFile,"rb")
-        except Exception, e:
+            fIn = file(sFile, "rb")
+        except Exception:
             self._clear_column_selectors()
             return
 
         try:
             sLine1 = fIn.next()
             sLine2 = fIn.next()
-        except Exception, e:
+        except Exception:
             fIn.close()
             self._clear_column_selectors()
             return
@@ -138,25 +153,25 @@ class CSVImporter(CardListPlugin):
         fIn.close()
 
         try:
-            oCsv = csv.reader([sLine1,sLine2])
+            oCsv = csv.reader([sLine1, sLine2])
             aRow = oCsv.next()
             sSample = sLine1 + "\n" + sLine2
             oSniff = csv.Sniffer()
             bHasHeader = oSniff.has_header(sSample)
-        except (Exception, csv.Error), e:
+        except (Exception, csv.Error):
             self._clear_column_selectors()
             return
 
         if bHasHeader:
-            aColumns = [(-1,'-')] + list(enumerate(aRow))
+            aColumns = [(-1, '-')] + list(enumerate(aRow))
         else:
-            aColumns = [(-1,'-')] + [(i,str(i)) for i in range(len(aRow))]
+            aColumns = [(-1, '-')] + [(i, str(i)) for i in range(len(aRow))]
 
-        self.oDlg.set_response_sensitive(gtk.RESPONSE_OK,True)
+        self.oDlg.set_response_sensitive(gtk.RESPONSE_OK, True)
         self._set_column_selectors(aColumns)
 
     def _create_column_selector(self):
-        oListStore = gtk.ListStore(gobject.TYPE_INT,gobject.TYPE_STRING)
+        oListStore = gtk.ListStore(gobject.TYPE_INT, gobject.TYPE_STRING)
         oComboBox = gtk.ComboBox(oListStore)
         oCell = gtk.CellRendererText()
         oComboBox.pack_start(oCell, True)
@@ -165,10 +180,10 @@ class CSVImporter(CardListPlugin):
 
     def _clear_column_selectors(self):
         # can't click ok until a valid CSV file is chosen
-        self.oDlg.set_response_sensitive(gtk.RESPONSE_OK,False)
-        self._set_column_selectors([(-1,'-')])
+        self.oDlg.set_response_sensitive(gtk.RESPONSE_OK, False)
+        self._set_column_selectors([(-1, '-')])
 
-    def _set_column_selectors(self,aColumns):
+    def _set_column_selectors(self, aColumns):
         for oCombo in (self.oCardNameCombo, self.oCountCombo,
                        self.oExpansionCombo):
             oListStore = oCombo.get_model()
@@ -178,12 +193,15 @@ class CSVImporter(CardListPlugin):
                 oListStore.set(oIter, 0, iRow, 1, sHeading)
             oCombo.set_active_iter(oListStore.get_iter_root())
 
-    def handle_response(self,oWidget,oResponse):
+    # pylint: disable-msg=W0613
+    # oWidget required by function signature
+    def handle_response(self, oWidget, oResponse):
         if oResponse == gtk.RESPONSE_OK:
             for oBut in self._oFirstBut.get_group():
                 sTypeName = oBut.get_label()
                 if oBut.get_active():
-                    iFileType, cCardSet = self._dCSVImportDestinations[sTypeName]
+                    iFileType, cCardSet = \
+                            self._dCSVImportDestinations[sTypeName]
                     break
 
             aCols = []
@@ -194,7 +212,7 @@ class CSVImporter(CardListPlugin):
                 if oIter is None:
                     aCols.append(None)
                 else:
-                    iVal = oModel.get_value(oIter,0)
+                    iVal = oModel.get_value(oIter, 0)
                     if iVal < 0:
                         aCols.append(None)
                     else:
@@ -203,7 +221,8 @@ class CSVImporter(CardListPlugin):
             iCardNameColumn, iCountColumn, iExpansionColumn = aCols
 
             if iCardNameColumn is None or iCountColumn is None:
-                sMsg = "Importing a CSV file requires valid columns for both the card names and card counts."
+                sMsg = "Importing a CSV file requires valid columns for both" \
+                        " the card names and card counts."
                 do_complaint_error(sMsg)
                 self.oDlg.run()
                 return
@@ -217,7 +236,8 @@ class CSVImporter(CardListPlugin):
                     self.oDlg.run()
                     return
                 if cCardSet.selectBy(name=sCardSetName).count() != 0:
-                    sMsg = "%s '%s' already exists." % (sTypeName, sCardSetName)
+                    sMsg = "%s '%s' already exists." % (sTypeName,
+                            sCardSetName)
                     do_complaint_error(sMsg)
                     self.oDlg.destroy()
                     return
@@ -230,16 +250,18 @@ class CSVImporter(CardListPlugin):
 
             sFile = self.oFileChooser.get_filename()
 
+            # pylint: disable-msg=W0703
+            # We want to catch all exceptions here
             try:
-                fIn = file(sFile,"rb")
-            except:
+                fIn = file(sFile, "rb")
+            except Exception:
                 do_complaint_error("Could not open file '%s'." % sFile)
                 self.oDlg.destroy()
                 return
 
             try:
                 oParser.parse(fIn, sCardSetName, oCardLookup=self.cardlookup)
-            except LookupFailed, e:
+            except LookupFailed:
                 fIn.close()
                 self.oDlg.destroy()
                 return
