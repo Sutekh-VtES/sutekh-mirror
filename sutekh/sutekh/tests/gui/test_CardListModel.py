4f26933c40607070ef4f0971c38d3daee3e9cfdc
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-06-20 21:30:20 +0000

    Start test case for the card model

---
+++ sutekh/sutekh/tests/gui/test_CardListModel.py
@@ -0,0 +1,40 @@
+# test_CardListModel.py
+# -*- coding: utf8 -*-
+# vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
+# Copyright 2008 Neil Muller <drnlmuller+sutekh@gmail.com>
+# GPL - see COPYING for details
+
+"""Tests the Card List Model"""
+
+from sutekh.tests.TestCore import SutekhTest
+from sutekh.core.SutekhObjects import AbstractCard
+from sutekh.gui.CardListModel import CardListModel, CardListModelListener
+import unittest
+
+class TestListener(CardListModelListener):
+    """Listener used in the test cases."""
+    def __init__(self):
+        self.bLoadCalled = False
+        self.aCards = []
+
+    def load(self, aAbsCards):
+        """Called when the model is loaded."""
+        self.bLoadCalled = True
+        self.aCards = aAbsCards
+
+class CardListModelTests(SutekhTest):
+    """Class for the test cases"""
+
+    def test_basic(self):
+        """Set of simple tests of the Card List Model"""
+        oModel = CardListModel()
+        oListener = TestListener()
+        oModel.load()
+        self.assertFalse(oListener.bLoadCalled)
+        oModel.add_listener(oListener)
+        oModel.load()
+        self.assertTrue(oListener.bLoadCalled)
+        # FIXME: Test the rest of the functionality
+
+if __name__ == "__main__":
+    unittest.main()
