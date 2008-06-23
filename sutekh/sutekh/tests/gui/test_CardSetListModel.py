c1ab5ec9df3eed85255485b9055a8dfbc8320a96
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-06-23 11:55:16 +0000

    Fix commit thinko which left the file names swapped

--- sutekh/sutekh/tests/gui/test_CardSetListModel.py
+++ sutekh/sutekh/tests/gui/test_CardSetListModel.py
@@ -1,4 +1,4 @@
-# test_CardListModel.py
+# test_CardSetListModel.py
 # -*- coding: utf8 -*-
 # vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
 # Copyright 2008 Neil Muller <drnlmuller+sutekh@gmail.com>
@@ -7,10 +7,12 @@
 """Tests the Card List Model"""
 
 from sutekh.tests.TestCore import SutekhTest
-from sutekh.gui.CardListModel import CardListModel, CardListModelListener
+from sutekh.gui.CardListModel import CardListModelListener
+from sutekh.gui.CardSetListModel import CardSetCardListModel
+from sutekh.core.SutekhObjects import PhysicalCardSet
 import unittest
 
-class TestListener(CardListModelListener):
+class CardSetListener(CardListModelListener):
     """Listener used in the test cases."""
     # pylint: disable-msg=W0231
     # CardListModelListener has no __init__
@@ -23,18 +25,21 @@ class TestListener(CardListModelListener):
         self.bLoadCalled = True
         self.aCards = aAbsCards
 
-class CardListModelTests(SutekhTest):
+class CardSetListModelTests(SutekhTest):
     """Class for the test cases"""
 
     def test_basic(self):
         """Set of simple tests of the Card List Model"""
-        oModel = CardListModel()
-        oListener = TestListener()
+        sName = 'Test 1'
+        oPCS = PhysicalCardSet(name=sName)
+        oModel = CardSetCardListModel(sName)
+        oListener = CardSetListener()
         oModel.load()
         self.assertFalse(oListener.bLoadCalled)
         oModel.add_listener(oListener)
         oModel.load()
         self.assertTrue(oListener.bLoadCalled)
+        self.assertEquals(len(oListener.aCards), 0)
         # FIXME: Test the rest of the functionality
 
 if __name__ == "__main__":
