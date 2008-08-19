73f67ea0bafdc74bf9c63730e298d973300caafe
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-08-19 14:17:56 +0000

    Add writers for the FELDB formats

---
+++ sutekh/sutekh/io/WriteELDBDeckFile.py
@@ -0,0 +1,80 @@
+# WriteELDBDeckFile.py
+# -*- coding: utf8 -*-
+# vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
+# Copyright 2008 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
+
+"""Writer for the FELDB deck format.
+
+   Example:
+
+   "A Deck"
+   "Author"
+   "Description"
+   2
+   "Alexandra"
+   "Bronwen"
+   5
+   "Aire of Elation"
+   "Aire of Elation"
+   "Social Charm"
+   "Social Charm"
+   "Social Charm"
+   """
+
+import unicodedata
+
+def _type_of_card(oCard):
+    """Return either Crypt or Library as required."""
+    sType = list(oCard.cardtype)[0].name
+    if sType == "Vampire" or sType == "Imbued":
+        return "Crypt"
+    else:
+        return "Library"
+
+def _norm_name(sName):
+    """Remove accents from the name"""
+    return unicodedata.normalize('NFKD', sName).encode('ascii','ignore')
+
+class WriteELDBDeckFile(object):
+    """Create a string in ELDB deck format representing a card set."""
+
+    # pylint: disable-msg=R0201
+    # method for consistency with the other methods
+    def _gen_header(self, oCardSet):
+        """Generate an ELDB deck file header."""
+        return '"%s"\n' \
+               '"%s"\n' \
+               '"%s"\n' % (oCardSet.name, oCardSet.author, oCardSet.comment)
+
+    def _gen_inv(self, oCardSet):
+        """Process the card set, creating the lines as needed"""
+        aCrypt = []
+        aLib = []
+        for oCard in oCardSet.cards:
+            oAbsCard = oCard.abstractCard
+            sType = list(oAbsCard.cardtype)[0].name
+            sName = _norm_name(oAbsCard.name)
+            if sType == "Vampire" or sType == "Imbued":
+                aCrypt.append(sName)
+            else:
+                aLib.append(sName)
+        aCrypt.sort()
+        aLib.sort()
+        sResult = "%d\n" % len(aCrypt)
+        for sName in aCrypt:
+            sResult += '"%s"\n' % sName
+        sResult += "%d\n" % len(aLib)
+        for sName in aLib:
+            sResult += '"%s"\n' % sName
+        return sResult
+
+    # pylint: enable-msg=R0201
+
+    def write(self, fOut, oCardSet):
+        """Takes file object + card set to write, and writes an ELDB inventory
+           representing the deck"""
+        fOut.write(self._gen_header(oCardSet))
+        fOut.write(self._gen_inv(oCardSet))
+
+    # pylint: enable-msg=R0913
