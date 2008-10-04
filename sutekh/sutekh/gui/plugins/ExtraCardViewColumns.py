dd53e75d86f8aaa2072ef4f07d594f30d54ebf73
Neil Muller <drnlmuller+sutekh@gmail.com>
2008-10-04 09:49:02 +0000

    Enable icons in the 'extra card column' view

--- sutekh/sutekh/gui/plugins/ExtraCardViewColumns.py
+++ sutekh/sutekh/gui/plugins/ExtraCardViewColumns.py
@@ -5,9 +5,11 @@
 # GPL - see COPYING for details
 """Disply extra columns in the tree view"""
 
-import gtk, pango
+import gtk
 from sutekh.gui.PluginManager import CardListPlugin
 from sutekh.gui.SutekhDialog import SutekhDialog
+from sutekh.gui.CellRendererIcons import CellRendererIcons, SHOW_TEXT_ONLY, \
+        SHOW_ICONS_ONLY, SHOW_ICONS_AND_TEXT
 from sutekh.core.SutekhObjects import AbstractCard, PhysicalCard, \
            PhysicalCardSet
 from sqlobject import SQLObjectNotFound
@@ -27,6 +29,12 @@ class ExtraCardViewColumns(CardListPlugin):
             'Expansions' : 600,
             'Group' : 40,
             'Capacity' : 40
+            }
+
+    _dModes = {
+            'Show Icons and Names' : SHOW_ICONS_AND_TEXT,
+            'Show Icons only' : SHOW_ICONS_ONLY,
+            'Show Text only' : SHOW_TEXT_ONLY,
             }
 
     # pylint: disable-msg=W0142
@@ -50,6 +58,8 @@ class ExtraCardViewColumns(CardListPlugin):
         self._dSortDataFuncs['Capacity'] = self._get_data_capacity
 
         self._dCardCache = {} # Used for sorting
+        self._iShowMode = SHOW_ICONS_AND_TEXT
+        self._oFirstBut = None
     # pylint: enable-msg=W0142
 
     # Rendering Functions
@@ -73,93 +83,121 @@ class ExtraCardViewColumns(CardListPlugin):
         else:
             return None
 
-
     # pylint: disable-msg=R0201, W0613
     # Making these functions for clarity
     # several unused paramaters due to function signatures
-    def _get_data_cardtype(self, oCard):
+    # The bGetIcons parameter is needed to avoid icon lookups, etc when
+    # sorting
+    def _get_data_cardtype(self, oCard, bGetIcons=True):
         """Return the card type"""
         if not oCard is None:
             aTypes = [x.name for x in oCard.cardtype]
             aTypes.sort()
-            return ",".join(aTypes)
-        return ""
+            aIcons = []
+            if bGetIcons:
+                dIcons = self.iconmanager.get_icon_list(oCard.cardtype)
+                if dIcons:
+                    aIcons = [dIcons[x] for x in aTypes]
+            return aTypes, aIcons
+        return [], []
 
     def _render_card_type(self, oColumn, oCell, oModel, oIter):
         """display the card type(s)"""
         oCard = self._get_card(oIter)
-        oCell.set_property("text", self._get_data_cardtype(oCard))
+        aText, aIcons = self._get_data_cardtype(oCard, True)
+        oCell.set_data(aText, aIcons, self._iShowMode)
 
-    def _get_data_clan(self, oCard):
+    def _get_data_clan(self, oCard, bGetIcons=True):
         """get the clan for the card"""
         if not oCard is None:
             aClans = [x.name for x in oCard.clan]
             aClans.sort()
-            return ",".join(aClans)
-        return ""
+            aIcons = []
+            if bGetIcons:
+                dIcons = self.iconmanager.get_icon_list(oCard.clan)
+                if dIcons:
+                    aIcons = [dIcons[x] for x in aClans]
+            return " /|".join(aClans).split("|"), aIcons
+        return [], []
 
     def _render_clan(self, oColumn, oCell, oModel, oIter):
         """display the clan"""
         oCard = self._get_card(oIter)
-        oCell.set_property("text", self._get_data_clan(oCard))
+        aText, aIcons = self._get_data_clan(oCard)
+        oCell.set_data(aText, aIcons, self._iShowMode)
 
-    def _get_data_disciplines(self, oCard):
+    def _get_data_disciplines(self, oCard, bGetIcons=True):
         """get disipline info for card"""
         if not oCard is None:
-            aDis = [(oP.level != 'superior') and oP.discipline.name
-                    or oP.discipline.name.upper() for oP in oCard.discipline]
-            aDis.sort()
-            return ",".join(aDis)
-        return ""
+            # There are better ways of doing this, but this is 1st draft
+            # attempt, so we'll do the firt that occurs to me
+            aInfo = [((oP.level != 'superior') and oP.discipline.name or
+                oP.discipline.name.upper(), oP.discipline.name) for
+                oP in oCard.discipline]
+            if aInfo:
+                aInfo.sort(key=lambda x: x[0])
+                if bGetIcons:
+                    dIcons = self.iconmanager.get_icon_list(oCard.discipline)
+                    aIcons = [dIcons[x[1]] for x in aInfo]
+                else:
+                    aIcons = []
+                aDis = ", ".join([x[0] for x in aInfo]).split(" ")
+                return aDis, aIcons
+        return [], []
 
     def _render_disciplines(self, oColumn, oCell, oModel, oIter):
         """display the card disciplines"""
         oCard = self._get_card(oIter)
-        oCell.set_property("text", self._get_data_disciplines(oCard))
+        aText, aIcons = self._get_data_disciplines(oCard)
+        oCell.set_data(aText, aIcons, self._iShowMode)
 
-    def _get_data_expansions(self, oCard):
+    def _get_data_expansions(self, oCard, bGetIcons=True):
         """get expansion info"""
         if not oCard is None:
             aExp = [oP.expansion.shortname + "(" + oP.rarity.name + ")" for
                     oP in oCard.rarity]
             aExp.sort()
-            return ",".join(aExp)
-        return ""
+            aIcons = []
+            if bGetIcons:
+                aIcons = [None]*len(aExp)
+            return aExp, aIcons
+        return [], []
 
     def _render_expansions(self, oColumn, oCell, oModel, oIter):
         """Display expanson info"""
         oCard = self._get_card(oIter)
-        oCell.set_property("text", self._get_data_expansions(oCard))
+        aText, aIcons = self._get_data_expansions(oCard)
+        oCell.set_data(aText, aIcons, self._iShowMode)
 
-    def _get_data_group(self, oCard):
+    def _get_data_group(self, oCard, bGetIcons=True):
         """get the group info for the card"""
         if not oCard is None and not oCard.group is None:
-            return oCard.group
-        return -1
+            return oCard.group, [None]
+        return -1, [None]
 
     def _render_group(self, oColumn, oCell, oModel, oIter):
         """Display the group info"""
         oCard = self._get_card(oIter)
-        iGrp = self._get_data_group(oCard)
+        iGrp, aIcons = self._get_data_group(oCard)
         if iGrp != -1:
-            oCell.set_property("text", str(iGrp))
+            oCell.set_data([str(iGrp)], aIcons, SHOW_TEXT_ONLY)
         else:
-            oCell.set_property("text", "")
+            oCell.set_data([""], aIcons, SHOW_TEXT_ONLY)
 
-    def _get_data_capacity(self, oCard):
+    def _get_data_capacity(self, oCard, bGetIcons=True):
         """Get the cards capacity"""
         if not oCard is None and not oCard.capacity is None:
-            return oCard.capacity
-        return -1
+            return oCard.capacity, [None]
+        return -1, [None]
 
     def _render_capacity(self, oColumn, oCell, oModel, oIter):
         """Display capacity in the column"""
         oCard = self._get_card(oIter)
-        iCap = self._get_data_capacity(oCard)
+        iCap, aIcons = self._get_data_capacity(oCard)
         if iCap != -1:
-            oCell.set_property("text", str(iCap))
+            oCell.set_data([str(iCap)], aIcons, SHOW_TEXT_ONLY)
         else:
-            oCell.set_property("text", "")
+            oCell.set_data([""], aIcons, SHOW_TEXT_ONLY)
 
     # pylint: enable-msg=R0201
     # Dialog and Menu Item Creation
@@ -178,78 +216,7 @@ class ExtraCardViewColumns(CardListPlugin):
         oDlg = self.make_dialog()
         oDlg.run()
 
-    # pylint: enable-msg=W0613
-
-    def make_dialog(self):
-        """Create the column selection dialog"""
-        sName = "Select Extra Columns ..."
-
-        oDlg = SutekhDialog(sName, self.parent,
-                gtk.DIALOG_DESTROY_WITH_PARENT)
-        oDlg.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
-        oDlg.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
-
-        oDlg.connect("response", self.handle_response)
-
-        # pylint: disable-msg=W0201
-        # no point in defining this in __init__
-        self._aButtons = []
-        aColsInUse = self.get_cols_in_use()
-        for sName in self._dCols:
-            oBut = gtk.CheckButton(sName)
-            oBut.set_active(sName in aColsInUse)
-            # pylint: disable-msg=E1101
-            # pylint doesn't detect vbox's methods
-            oDlg.vbox.pack_start(oBut)
-            self._aButtons.append(oBut)
-
-        oDlg.show_all()
-
-        return oDlg
-
-    # Actions
-
-    def handle_response(self, oDlg, oResponse):
-        """Handle user response from the dialog"""
-        if oResponse == gtk.RESPONSE_CANCEL:
-            oDlg.destroy()
-        elif oResponse == gtk.RESPONSE_OK:
-            aColsInUse = []
-            for oBut in self._aButtons:
-                if oBut.get_active():
-                    sLabel = oBut.get_label()
-                    aColsInUse.append(sLabel)
-            self.set_cols_in_use(aColsInUse)
-            oDlg.destroy()
-
-    def set_cols_in_use(self, aCols):
-        """Add columns to the view"""
-        # pylint: disable-msg=W0612
-        # iDir is returned, although we don't need it
-        iSortCol, iDir = self.model.get_sort_column_id()
-        # pylint: enable-msg=W0612
-        if iSortCol is not None and iSortCol > 1:
-            # We're changing the columns, so restore sorting to default
-            self.model.set_sort_column_id(0, 0)
-
-        for oCol in self._get_col_objects():
-            self.view.remove_column(oCol)
-
-        for iNum, sCol in enumerate(aCols):
-            oCell = gtk.CellRendererText()
-            oCell.set_property('style', pango.STYLE_ITALIC)
-            oColumn = gtk.TreeViewColumn(sCol, oCell)
-            oColumn.set_cell_data_func(oCell, self._dCols[sCol])
-            oColumn.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
-            oColumn.set_fixed_width(self._dWidths.get(sCol, 100))
-            oColumn.set_resizable(True)
-            self.view.insert_column(oColumn, iNum + 3)
-            oColumn.set_sort_column_id(iNum + 3)
-            self.model.set_sort_func(iNum + 3, self.sort_column,
-                    self._dSortDataFuncs[sCol])
-
-    # pylint: disable-msg=W0613
-    # oModel required by gtk's function signature
+    # W0613: oModel required by gtk's function signature
     def sort_column(self, oModel, oIter1, oIter2, oGetData):
         """Stringwise comparision of oIter1 and oIter2.
 
@@ -268,8 +235,16 @@ class ExtraCardViewColumns(CardListPlugin):
             else:
                 iRes = 0
             return iRes
-        oVal1 = oGetData(oCard1)
-        oVal2 = oGetData(oCard2)
+        # Don't use icon info when sorting
+        oVal1 = oGetData(oCard1, False)[0]
+        oVal2 = oGetData(oCard2, False)[0]
+        # convert to string for sorting
+        if isinstance(oVal1, list):
+            oVal1 = " ".join(oVal1)
+            oVal2 = " ".join(oVal2)
+        else:
+            oVal1 = str(oVal1)
+            oVal2 = str(oVal2)
         if oVal1 < oVal2:
             iRes = -1
         elif oVal1 > oVal2:
@@ -280,7 +255,95 @@ class ExtraCardViewColumns(CardListPlugin):
         else:
             iRes = 1 # Card names assumed to be unique
         return iRes
+
     # pylint: enable-msg=W0613
+
+    def make_dialog(self):
+        """Create the column selection dialog"""
+        sName = "Select Extra Columns ..."
+
+        oDlg = SutekhDialog(sName, self.parent,
+                gtk.DIALOG_DESTROY_WITH_PARENT)
+        oDlg.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+        oDlg.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
+
+        oDlg.connect("response", self.handle_response)
+
+        # pylint: disable-msg=W0201
+        # no point in defining this in __init__
+        self._aButtons = []
+        aColsInUse = self.get_cols_in_use()
+        # pylint: disable-msg=E1101
+        # pylint doesn't detect vbox's methods
+        for sName in self._dCols:
+            oBut = gtk.CheckButton(sName)
+            oBut.set_active(sName in aColsInUse)
+            oDlg.vbox.pack_start(oBut)
+            self._aButtons.append(oBut)
+
+        oDlg.vbox.pack_start(gtk.HSeparator())
+
+        oIter = self._dModes.iterkeys()
+        for sName in oIter:
+            self._oFirstBut = gtk.RadioButton(None, sName, False)
+            if self._iShowMode == self._dModes[sName]:
+                self._oFirstBut.set_active(True)
+            oDlg.vbox.pack_start(self._oFirstBut, expand=False)
+            break
+        for sName in oIter:
+            oBut = gtk.RadioButton(self._oFirstBut, sName)
+            oDlg.vbox.pack_start(oBut, expand=False)
+            if self._iShowMode == self._dModes[sName]:
+                oBut.set_active(True)
+
+        oDlg.show_all()
+
+        return oDlg
+
+    # Actions
+
+    def handle_response(self, oDlg, oResponse):
+        """Handle user response from the dialog"""
+        if oResponse == gtk.RESPONSE_CANCEL:
+            oDlg.destroy()
+        elif oResponse == gtk.RESPONSE_OK:
+            aColsInUse = []
+            for oBut in self._aButtons:
+                if oBut.get_active():
+                    sLabel = oBut.get_label()
+                    aColsInUse.append(sLabel)
+            for oBut in self._oFirstBut.get_group():
+                sName = oBut.get_label()
+                if oBut.get_active():
+                    self._iShowMode = self._dModes[sName]
+
+            self.set_cols_in_use(aColsInUse)
+            oDlg.destroy()
+
+    def set_cols_in_use(self, aCols):
+        """Add columns to the view"""
+        # pylint: disable-msg=W0612
+        # iDir is returned, although we don't need it
+        iSortCol, iDir = self.model.get_sort_column_id()
+        # pylint: enable-msg=W0612
+        if iSortCol is not None and iSortCol > 1:
+            # We're changing the columns, so restore sorting to default
+            self.model.set_sort_column_id(0, 0)
+
+        for oCol in self._get_col_objects():
+            self.view.remove_column(oCol)
+
+        for iNum, sCol in enumerate(aCols):
+            oCell = CellRendererIcons()
+            oColumn = gtk.TreeViewColumn(sCol, oCell)
+            oColumn.set_cell_data_func(oCell, self._dCols[sCol])
+            oColumn.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
+            oColumn.set_fixed_width(self._dWidths.get(sCol, 100))
+            oColumn.set_resizable(True)
+            self.view.insert_column(oColumn, iNum + 3)
+            oColumn.set_sort_column_id(iNum + 3)
+            self.model.set_sort_func(iNum + 3, self.sort_column,
+                    self._dSortDataFuncs[sCol])
 
     def get_cols_in_use(self):
         """Get which extra columns have been added to view"""
