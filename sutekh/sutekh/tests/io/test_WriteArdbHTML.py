77efb7e383eafdde29292edf272cc050780a5cd8
Neil Muller <drnlmuller+sutekh@gmail.com>
2009-01-07 15:30:11 +0000

    Add Secret Library output to HTML writer test

--- sutekh/sutekh/tests/io/test_WriteArdbHTML.py
+++ sutekh/sutekh/tests/io/test_WriteArdbHTML.py
@@ -233,7 +233,119 @@ sExpected2 = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 </html>""" % (sHTMLStyle, SutekhInfo.VERSION_STR,
         time.strftime('%Y-%m-%d', time.localtime()))
 
-
+sExpected3 = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta content="text/html; charset=&quot;us-ascii&quot;" http-equiv="content-type" />
+    <style type="text/css">%s</style>
+    <title>VTES deck : Test Set 1 by A test author</title>
+  </head><body>
+    <div id="info">
+      <h1 id="nametitle">
+        <span>Deck Name :</span>
+        <span class="value" id="namevalue">Test Set 1</span>
+      </h1><h2 id="authortitle">
+        <span>Author : </span>
+        <span class="value" id="authornamevalue">A test author</span>
+      </h2><h2 id="description">
+        <span>Description : </span>
+      </h2><p>
+        <span class="value" id="descriptionvalue">A test comment</span>
+      </p>
+    </div><div id="crypt">
+      <h3 id="crypttitle">
+        <span>Crypt</span>
+        <span>[1 vampires] Capacity min : 4 max : 4 average : 4.00</span>
+      </h3><div id="crypttable">
+        <table summary="Crypt card table">
+          <tbody>
+            <tr>
+              <td>
+                <span class="tablevalue">1x</span>
+              </td><td>
+                <span class="tablevalue">
+                  <a href="http://www.secretlibrary.info/?crypt=Abebe">Abebe</a>
+                </span>
+              </td><td />
+              <td>
+                <span class="tablevalue">4</span>
+              </td><td>
+                <span class="tablevalue">nec obf thn</span>
+              </td><td />
+              <td>
+                <span class="tablevalue">Samedi (group 4)</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div><div id="library">
+      <h3 id="librarytitle">
+        <span>Library</span>
+        <span class="stats" id="librarystats">[4 cards]</span>
+      </h3><div class="librarytable">
+        <h4 class="librarytype">
+          <span>Action</span>
+          <span class="stats">[1]</span>
+        </h4><table summary="Library card table">
+          <tbody>
+            <tr>
+              <td>
+                <span class="tablevalue">1x</span>
+              </td><td>
+                <span class="tablevalue">
+                  <a href="http://www.secretlibrary.info/?lib=Abbot">Abbot</a>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table><h4 class="librarytype">
+          <span>Equipment</span>
+          <span class="stats">[2]</span>
+        </h4><table summary="Library card table">
+          <tbody>
+            <tr>
+              <td>
+                <span class="tablevalue">1x</span>
+              </td><td>
+                <span class="tablevalue">
+                  <a href="http://www.secretlibrary.info/?lib=.44+Magnum">.44 Magnum</a>
+                </span>
+              </td>
+            </tr><tr>
+              <td>
+                <span class="tablevalue">1x</span>
+              </td><td>
+                <span class="tablevalue">
+                  <a href="http://www.secretlibrary.info/?lib=AK-47">AK-47</a>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table><h4 class="librarytype">
+          <span>Master</span>
+          <span class="stats">[1]</span>
+        </h4><table summary="Library card table">
+          <tbody>
+            <tr>
+              <td>
+                <span class="tablevalue">1x</span>
+              </td><td>
+                <span class="tablevalue">
+                  <a href="http://www.secretlibrary.info/?lib=Abombwe">Abombwe</a>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div><div>
+      <span class="generator">Crafted with : Sutekh [ %s ]. [ %s ]</span>
+    </div>
+  </body>
+</html>""" % (sHTMLStyle, SutekhInfo.VERSION_STR,
+        time.strftime('%Y-%m-%d', time.localtime()))
 
 
 class ARDBHTMLWriterTests(SutekhTest):
@@ -288,6 +400,15 @@ class ARDBHTMLWriterTests(SutekhTest):
 
         self.assertEqual(sData, sExpected2)
 
+        oWriter = WriteArdbHTML('Secret Library')
+        sTempFileName =  self._create_tmp_file()
+        oWriter.write(sTempFileName, oPhysCardSet1, oPhysCardSet1.cards, False)
+
+        fIn = open(sTempFileName, 'rU')
+        sData = fIn.read()
+        fIn.close()
+
+        self.assertEqual(sData, sExpected3)
 
 
 if __name__ == "__main__":
