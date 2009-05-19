1882b579141850efeff4da377a97cd9ec173fdb7
Neil Muller <drnlmuller+sutekh@gmail.com>
2009-05-19 13:57:37 +0000

    Make output less noisy for files that pass

--- tools/check_single_line_docstring_format.sh
+++ tools/check_single_line_docstring_format.sh
@@ -4,6 +4,7 @@
 # We rely on pygettext to pull out the doc-strings, and grep
 # to check for """
 # GPL license, see COPYRIGHT file for details.
+
 pyfile=${1?'No file specified'}
 if [ ! -f $pyfile ]; then
    echo "No file found" >&2
@@ -14,7 +15,9 @@ pygettext -D -o - $pyfile \
     | grep ^msgid \
     | grep -v \"\"  \
     | cut -d " " -f 2-  > $DOC_LIST
-echo "$pyfile: Possible single line docstrings not using \"\"\":"
-grep -n -F -f $DOC_LIST $pyfile | grep -v \"\"\"
+if grep -F -f $DOC_LIST $pyfile | grep -q -F -v \"\"\"; then
+    echo "$pyfile: Possible single line docstrings not using \"\"\":"
+    grep -n -F -f $DOC_LIST $pyfile | grep -F -v \"\"\"
+fi
 rm $DOC_LIST
 
