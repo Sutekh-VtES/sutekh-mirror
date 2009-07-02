93f40940631824a6f7722aee16f3b662eb02552c
Neil Muller <drnlmuller+sutekh@gmail.com>
2009-07-02 09:55:26 +0000

    Fix header & copyright date

--- sutekh/sutekh/gui/plugins/CardSetExport.py
+++ sutekh/sutekh/gui/plugins/CardSetExport.py
@@ -1,7 +1,7 @@
-# CardSetExportJOL.py
+# CardSetExport.py
 # -*- coding: utf8 -*-
 # vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
-# Copyright 2008 Neil Muller <drnlmuller+sutekh@gmail.com>
+# Copyright 2008, 2009 Neil Muller <drnlmuller+sutekh@gmail.com>
 # GPL - see COPYING for details
 
 """Plugin for exporting to standard writers"""
