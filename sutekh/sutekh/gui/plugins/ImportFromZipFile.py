ba275a0e72f353f99291848a944b47cf72792975
Neil Muller <drnlmuller+sutekh@gmail.com>
2011-01-31 12:27:28 +0000

    Fix name escaping bug. Add select all / unselect all buttons and choosing  forced replace or auto rename option

--- sutekh/sutekh/gui/plugins/ImportFromZipFile.py
+++ sutekh/sutekh/gui/plugins/ImportFromZipFile.py
@@ -15,18 +15,23 @@ from sutekh.gui.ProgressDialog import ProgressDialog, SutekhCountLogHandler
 from sutekh.gui.ScrolledList import ScrolledList
 from sutekh.io.ZipFileWrapper import ZipFileWrapper
 from sutekh.gui.GuiCardSetFunctions import reparent_all_children, \
-        update_open_card_sets, get_import_name
+        update_open_card_sets, get_import_name, PROMPT, RENAME, REPLACE
 
 
 def _do_rename_parent(sOldName, sNewName, dRemaining):
     """Handle the renaming of a parent card set in the unprocessed list."""
     dResult = {}
     for sName, tInfo in dRemaining.iteritems():
-        if tInfo[2] == sOldName:
-            dResult[sName] = (tInfo[0], tInfo[1], sNewName)
+        if tInfo[3] == sOldName:
+            dResult[sName] = (tInfo[0], tInfo[1], tInfo[2], sNewName)
         else:
             dResult[sName] = tInfo
     return dResult
+
+
+def _set_selection(_oButton, oScrolledList, aData):
+    """Helper to manage changing the se;ection of the scrolled list"""
+    oScrolledList.set_selection(aData)
 
 
 class ImportFromZipFile(SutekhPlugin):
@@ -68,6 +73,10 @@ class ImportFromZipFile(SutekhPlugin):
             return
         oFile = ZipFileWrapper(sFilename)
         dList = oFile.get_all_entries()
+        dEscapedList = {}
+        for sName, tInfo in dList.iteritems():
+            dEscapedList[self.escape(sName)] = (sName, tInfo[0], tInfo[1],
+                    tInfo[2])
         dSelected = {}
         # Ask user to select entries to import
         oSelDlg = SutekhDialog("Select Card Sets to Import", self.parent,
@@ -79,17 +88,55 @@ class ImportFromZipFile(SutekhPlugin):
         # pylint: disable-msg=E1101
         # vbox confuses pylint
         oSelDlg.vbox.pack_start(oScrolledList)
-        oScrolledList.set_size_request(150, 300)
-        oScrolledList.fill_list(sorted(dList))
+        oScrolledList.set_size_request(450, 300)
+        oScrolledList.fill_list(sorted(dEscapedList))
+        # Add the various buttons
+        # Select all and unselect all
+        oSelectAll = gtk.Button('Select All')
+        oUnSelectAll = gtk.Button('Unselect All')
+        oSelectAll.connect('clicked', _set_selection, oScrolledList,
+                dEscapedList)
+        oUnSelectAll.connect('clicked', _set_selection, oScrolledList, [])
+        oSelectButtons = gtk.VBox(False, 2)
+        oSelectButtons.pack_start(oSelectAll, expand=False)
+        oSelectButtons.pack_start(oUnSelectAll, expand=False)
+        oPrompt = gtk.RadioButton(None, 'Always Ask', False)
+        oPrompt.set_active(True)
+        oReplace = gtk.RadioButton(oPrompt,
+                'Always replace with new card set', False)
+        oReplace.set_active(False)
+        oRename = gtk.RadioButton(oPrompt, 'Always create unique name', False)
+        oRename.set_active(False)
+        oRadioButs = gtk.VBox(False, 2)
+        oRadioLabel = gtk.Label()
+        oRadioLabel.set_markup('<b>How to handle card set name conflicts?</b>')
+        oRadioButs.pack_start(oRadioLabel)
+        oRadioButs.pack_start(oPrompt, expand=False)
+        oRadioButs.pack_start(oReplace, expand=False)
+        oRadioButs.pack_start(oRename, expand=False)
+
+        oButtons = gtk.HBox(False, 2)
+        oButtons.pack_start(oSelectButtons, expand=False)
+        oButtons.pack_start(oRadioButs)
+        oSelDlg.vbox.pack_start(oButtons, expand=False)
+
+        oSelDlg.show_all()
+
         oResponse = oSelDlg.run()
         # Extract selected cards from the dialog
         for sName in oScrolledList.get_selection():
-            dSelected[sName] = dList[sName]
+            dSelected[sName] = dEscapedList[sName]
         oSelDlg.destroy()
         if oResponse == gtk.RESPONSE_OK and len(dSelected) > 0:
-            self.do_read_list(oFile, dSelected)
+            if oRename.get_active():
+                iClashMode = RENAME
+            elif oReplace.get_active():
+                iClashMode = REPLACE
+            else:
+                iClashMode = PROMPT
+            self.do_read_list(oFile, dSelected, iClashMode)
 
-    def do_read_list(self, oFile, dSelected):
+    def do_read_list(self, oFile, dSelected, iClashMode):
         """Read the selected list of card sets"""
         oLogHandler = SutekhCountLogHandler()
         oProgressDialog = ProgressDialog()
@@ -101,19 +148,19 @@ class ImportFromZipFile(SutekhPlugin):
         oProgressDialog.show()
         bDone = False
         while not bDone:
-            dSelected = self._read_heart(oFile, dSelected, oLogger)
+            dSelected = self._read_heart(oFile, dSelected, oLogger, iClashMode)
             bDone = len(dSelected) == 0
         oProgressDialog.destroy()
 
-    def _read_heart(self, oFile, dSelected, oLogger):
+    def _read_heart(self, oFile, dSelected, oLogger, iClashMode):
         """Heart of the reading loop - ensure we read parents before
            children, and correct for renames that occur."""
         # pylint: disable-msg=W0703
         # we really do want all the exceptions
         dRemaining = {}
         dRenames = {}
-        for sName, tInfo in dSelected.iteritems():
-            sFilename, bParentExists, sParentName = tInfo
+        for sEscapedName, tInfo in dSelected.iteritems():
+            sName, sFilename, bParentExists, sParentName = tInfo
             # We aim to ensure that we always keep loaded card sets together,
             # so if the parent name clashes, we want to follow the renamed
             # parent, rather than be added to the existing card set of the
@@ -123,9 +170,10 @@ class ImportFromZipFile(SutekhPlugin):
                 if not bParentExists:
                     # The parent is in the list of cards to read, so it
                     # will exist when we get to it
-                    dRemaining[sName] = (sFilename, True, sParentName)
+                    dRemaining[sEscapedName] = (sName, sFilename, True,
+                            sParentName)
                 else:
-                    dRemaining[sName] = tInfo
+                    dRemaining[sEscapedName] = tInfo
                 continue
             elif sParentName is not None and sParentName in dRenames:
                 # Renamed parent, so adjust this - this will also correctly
@@ -140,7 +188,8 @@ class ImportFromZipFile(SutekhPlugin):
                 oCardSetHolder = oFile.read_single_card_set(sFilename)
                 oLogger.info('Read %s' % sName)
                 # Ask the user whether to rename, replace or cancel
-                oCardSetHolder, aChildren = get_import_name(oCardSetHolder)
+                oCardSetHolder, aChildren = get_import_name(
+                        oCardSetHolder, iClashMode)
                 dRenames[sName] = oCardSetHolder.name
                 # Check for cardsets that were waiting for us
                 dRemaining = _do_rename_parent(sName, oCardSetHolder.name,
