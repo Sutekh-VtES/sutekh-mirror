53f75934d9c842d8fef5f54b127ecbcbdc1c3cd4
Neil Muller <drnlmuller+sutekh@gmail.com>
2011-08-27 16:42:26 +0000

    Adapt EDLB name escaping to match observed behaviour

--- sutekh/sutekh/core/ELDBUtilities.py
+++ sutekh/sutekh/core/ELDBUtilities.py
@@ -10,6 +10,17 @@ import unicodedata
 from sutekh.core.SutekhObjects import AbstractCard, canonical_to_csv
 from sutekh.SutekhUtility import is_crypt_card
 
+SINGLE_QUOTE_NAMES=[
+        "Al-Muntathir, God's Witness",
+        "Crypt's Sons",
+        "Poacher's Hunting Ground",
+        "World's a Canvas, The",
+        "Joumlon's Axe",
+        "Mole's Tunneling",
+        "Three's a Crowd",
+        "Unleash Hell's Fury",
+        ]
+
 
 def type_of_card(oCard):
     """Return either Crypt or Library as required."""
@@ -22,12 +33,19 @@ def type_of_card(oCard):
 def norm_name(oCard):
     """Transform a card name to the ELDB equivalent"""
     sName = oCard.name
+    sType = oCard.cardtype[0].name
     if oCard.level is not None:
         sName = sName.replace("(Advanced)", "(ADV)")
+    # Handle annoying ELDB special cases
     if sName != "The Kikiyaon":
-        # Annoying ELDB special case
         sName = canonical_to_csv(sName)
-    sName = sName.replace("'", "`")
+    if sType == 'Imbued' or sName == 'Ondine "Boudicca" Sinclair':
+        # These are quoted differently from other ELDB names
+        sName = sName.replace('"', "'")
+    elif sName not in SINGLE_QUOTE_NAMES:
+        # usual ELDB quoting
+        sName = sName.replace('"', "`")
+        sName = sName.replace("'", "`")
     return unicodedata.normalize('NFKD', sName).encode('ascii', 'ignore')
 
 
