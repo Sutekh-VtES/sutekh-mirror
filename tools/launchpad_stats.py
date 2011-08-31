043431a922ef2e2c3f1bd0ea7118ac472af16d00
Simon Cross <hodgestar@gmail.com>
2011-08-31 11:32:59 +0000

    Script for showing launchpad download stats for Sutkeh.

---
+++ tools/launchpad_stats.py
@@ -0,0 +1,24 @@
+from launchpadlib.launchpad import Launchpad
+
+sApp = "sutekh-stats"
+sTeam = "sutekh"
+
+lp = Launchpad.login_anonymously(sApp)
+team = lp.people[sTeam]
+
+print "Showing download stats for team Sutekh's PPAs"
+print "---------------------------------------------"
+
+for ppa in team.ppas:
+    print "%s/%s:" % (sTeam, ppa.name)
+    binaries = ppa.getPublishedBinaries()
+    for bin in binaries:
+        print "  %s (%s):" % (bin.binary_package_name,
+                              bin.binary_package_version)
+        total = bin.getDownloadCount()
+        stats = bin.getDailyDownloadTotals()
+        print "    Total:", total
+        for date in sorted(stats.keys()):
+            print "    %s: %d" % (date, stats[date])
+
+print "---------------------------------------------"
