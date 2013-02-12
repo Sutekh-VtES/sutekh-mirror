5f6687d3c16c735996ea1c8e15b281add8ad16e6
Neil Muller <drnlmuller+sutekh@gmail.com>
2013-02-12 08:32:54 +0000

    Fetch rulings from bitbucket. Remove stale white-wolf.com urls.

--- sutekh/sutekh/io/WwFile.py
+++ sutekh/sutekh/io/WwFile.py
@@ -15,49 +15,8 @@ WW_CARDLIST_URL = "http://www.vekn.net/images/stories/downloads/cardlist.txt"
 EXTRA_CARD_URL = "http://bitbucket.org/hodgestar/sutekh-extras/" \
         "raw/tip/extra_list.txt"
 
-# pylint: disable-msg=C0103
-# We accept the overly long name here
-WW_CARDLIST_PER_LETTER_URLS = [
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist__",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_A",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_B",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_C",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_D",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_E",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_F",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_G",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_H",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_I",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_J",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_K",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_L",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_M",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_N",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_O",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_P",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_Q",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_R",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_S",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_T",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_U",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_V",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_W",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_X",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_Y",
-        "http://www.white-wolf.com/vtes/index.php?line=cardlist_Z",
-        ]
-
-# pylint: enable-msg=C0103
-
-WW_OLD_RULINGS_URL = "http://www.white-wolf.com/vtes/index.php?line=rulings"
-
-WW_RULINGS_URL = [
-        # We skip the general rulings, since we only want card rulings
-        #"http://www.vekn.net/index.php/general-rulings",
-        "http://www.vekn.net/index.php/card-rulings/card-rulings-a-to-f",
-        "http://www.vekn.net/index.php/card-rulings/card-rulings-g-to-o",
-        "http://www.vekn.net/index.php/card-rulings/card-rulings-p-to-z",
-        ]
+WW_RULINGS_URL = ["http://bitbucket.org/hodgestar/sutekh-extras/"
+        "raw/tip/Rulebooks/rulebook-data/rulings.html"]
 
 
 def guess_encoding(sData, sFile):
