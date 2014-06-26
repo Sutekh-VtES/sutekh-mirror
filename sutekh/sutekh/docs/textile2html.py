fcf8357671b778513a22904b2548e917b21cb8ba
Neil Muller <drnlmuller+sutekh@gmail.com>
2014-06-26 21:16:18 +0000

    Rework how filter html text is generated to avoid some repeated details

--- sutekh/sutekh/docs/textile2html.py
+++ sutekh/sutekh/docs/textile2html.py
@@ -29,6 +29,7 @@ modpath = os.path.join(os.path.dirname(__file__), '..', '..')
 oFile, sModname, oDescription = imp.find_module('sutekh', [modpath])
 sutekh_package = imp.load_module('sutekh', oFile, sModname, oDescription)
 Filters = sutekh_package.core.Filters
+FilterParser = sutekh_package.base.core.FilterParser
 
 
 HTML_HEADER = """
@@ -46,15 +47,64 @@ HTML_FOOTER = """
 </html>
 """
 
+# Sections for the filter help files
+
+FILTER_USAGE = """h2(#usage). Usage
+
+Filter elements and values for the filters can be dragged from the right
+hand pane to the filter to add them to the filter, and dragged from the filter
+to the right hand pane to remove them from the filter. Disabled filter
+elements or filter elements with no values set are ignored.
+"""
+
+FILTER_NAVIGATION = """h2(#navigation). Keyboard Navigation
+
+You can switch between the different filter lists using
+*&lt;Alt&gt;--&lt;Left&gt;* and *&lt;Alt&gt;--&lt;Right&gt;*.
+*&lt;Ctrl&gt;--&lt;Enter&gt;* will paste the currently selected list of values
+into the filter.  *&lt;Del&gt;* will delete a value or filter part from the
+filter *&lt;Ctrl&gt;--&lt;Space&gt;* will disable a filter part, while
+*&lt;Alt&gt;--&lt;Space&gt;* will negate it.
+"""
+
+FILTER_ELEMENTS = """h2(#elements). Filter Elements
+
+The available filtering options are listed below.  The first line of each item
+shows the description you'll see in the filter editor in bold.  The rest of the
+description describes the arguments the filter takes and the results it
+produces.
+"""
+
 # pylint: enable-msg=C0103
+
+
+class FilterGroup(object):
+    """Fake filter for Filter Group help."""
+
+    description = "Filter Group"
+    keyword = "filter_group"
+    helptext = ("A list of other filter elements.\nFilter element with holds"
+                " other filter elements. The result of the filter group is "
+                " the combination of all the filter elements as specified by"
+                " the Filter Group type.\n\n"
+                "The 4 Group types are:\n\n"
+                "* _All of_: This filter matches only if every filter in"
+                " the group matches.\n"
+                "* _Any of_: This filter matches if at least one of the"
+                " filters in the group matches.\n"
+                "* _Not all of_: This filter matches if at least one of the"
+                " filters in the group doesn't match.\n"
+                "* _Not any of_: This filter matches only if none of the"
+                " filters in the group match.\n")
 
 
 def textile2html(sText, dContext):
     """Convert a Textile markup string to an HTML file."""
     sHtml = ''.join([HTML_HEADER % dContext,
-            textile.textile(sText.replace('!Sutekh Version!',
-                'Sutekh %s' % SutekhInfo.BASE_VERSION_STR)),
-            HTML_FOOTER % dContext])
+                     textile.textile(sText.replace(
+                         '!Sutekh Version!',
+                         'Sutekh %s' % SutekhInfo.BASE_VERSION_STR)),
+                     HTML_FOOTER % dContext])
     sHtml = sHtml.replace('<br />', ' ')  # remove pesky linebreaks
     return sHtml
 
@@ -110,8 +160,15 @@ def convert(sTextileDir, sHtmlDir):
 def make_filter_txt(sDir):
     """Convert base filters into the approriate textile files"""
 
+    dSections = {
+        'Usage': FILTER_USAGE,
+        'Elements': FILTER_ELEMENTS,
+        'Navigation': FILTER_NAVIGATION,
+    }
+
     dFilters = {}
-    for oFilter in Filters.PARSER_FILTERS:
+    dFilters['Filter_Group'] = FilterGroup
+    for oFilter in FilterParser.PARSER_FILTERS:
         dFilters[oFilter.keyword] = oFilter
 
     for sTemplatePath in glob.glob(os.path.join(sDir, "*.tmpl")):
@@ -126,38 +183,61 @@ def make_filter_txt(sDir):
 
         for sLine in fTemplate.readlines():
             sKeyword = sLine.strip()
-            if sKeyword not in dFilters:
-                if sKeyword != '##toc':
-                    aOutput.append(sLine)
-                else:
-                    # We're going to insert new toc entries here
-                    iTocIndex = len(aOutput)
+            if sKeyword.startswith('!') and sKeyword.endswith('!'):
+                sKeyword = sKeyword.replace('!', '')
+            else:
+                # Not flagged as template relevant, so continue
+                aOutput.append(sLine)
                 continue
-            oFilter = dFilters[sKeyword]
+            if sKeyword == '#toc':
+                # We're going to insert new toc entries here
+                iTocIndex = len(aOutput)
+                continue
+            elif sKeyword in dFilters:
+                oFilter = dFilters[sKeyword]
 
-            # Generate toc entry
-            sDesc = oFilter.description
-            sLink = sKeyword.lower()
+                # Generate toc entry
+                sDesc = oFilter.description
+                sLink = sKeyword.lower()
 
-            aOutput.insert(iTocIndex, '## "%s":#%s\n' % (sDesc, sLink))
-            iTocIndex += 1
+                aOutput.insert(iTocIndex, '## "%s":#%s\n' %
+                               (sDesc, sLink))
+                iTocIndex += 1
 
-            aOutput.append('h3(#%s). %s\n\n' % (sLink, sDesc))
+                aOutput.append('h3(#%s). %s\n\n' % (sLink, sDesc))
 
-            try:
-                sInput, sRest = oFilter.helptext.split('\n', 1)
-            except ValueError:
-                print oFilter.keyword, oFilter.helptext
+                try:
+                    sInput, sRest = oFilter.helptext.split('\n', 1)
+                except ValueError:
+                    print 'Failed to extract filter details'
+                    print oFilter.keyword, oFilter.helptext
 
-            aOutput.append('*Parameters:* %s\n\n' % sInput)
-            aOutput.append(sRest)
-            aOutput.append('\n')
-            del dFilters[sKeyword]
+                aOutput.append('*Parameters:* %s\n\n' % sInput)
+                aOutput.append(sRest)
+                aOutput.append('\n')
+                if sKeyword != 'Filter_Group':
+                    del dFilters[sKeyword]
+            elif sKeyword in dSections:
+                sText = dSections[sKeyword]
+                # Generate toc entry
+                sLink = sKeyword.lower()
+                sDesc = sText.split('\n', 1)[0]  # extract first line
+                sDesc = sDesc.split('). ', 1)[1]  # extract description
+                aOutput.insert(iTocIndex, '# "%s":#%s\n' %
+                               (sDesc, sLink))
+                iTocIndex += 1
+                aOutput.append(sText)
+                aOutput.append('\n')
+            else:
+                print 'Unrecognised Keyword in template %s: %s' % (sBasename,
+                                                                   sKeyword)
 
         fTextile.write(''.join(aOutput))
 
+    if 'Filter_Group' in dFilters:
+        del dFilters['Filter_Group']
     if dFilters:
-        print 'The following filters have no entry in the template'
+        print 'The following filters have no entry in the templates.'
         print dFilters.keys()
 
 
