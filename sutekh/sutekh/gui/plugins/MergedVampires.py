b00b52d2661dba78fa4395380cbb85ec34ef4f6f
Neil Muller <drnlmuller+sutekh@gmail.com>
2014-10-23 09:00:40 +0000

    Add 'show merged' and 'show advanced' buttons to the base vampire. Pylint cleanups

--- sutekh/sutekh/gui/plugins/MergedVampires.py
+++ sutekh/sutekh/gui/plugins/MergedVampires.py
@@ -7,17 +7,22 @@
 
 from sutekh.base.core.BaseObjects import (PhysicalCardSet,
                                           IKeyword, IAbstractCard)
-from sutekh.core.SutekhObjects import (ITitle, ISect, IDisciplinePair)
+from sutekh.core.SutekhObjects import (ITitle, ISect, IDisciplinePair,
+                                       SutekhAbstractCard)
 from sutekh.gui.PluginManager import SutekhPlugin
 from sutekh.base.gui.MessageBus import MessageBus, CARD_TEXT_MSG
 from sutekh.base.gui.GuiUtils import make_markup_button
 from sutekh.base.Utility import normalise_whitespace
+
+from sqlobject import SQLObjectNotFound
 
 
 class FakeTitle(object):
     """Fake titles not in the database"""
 
     def __init__(self, sText):
+        # pylint: disable-msg=C0103
+        # We duplicate Title naming here
         self.name = sText
 
 
@@ -25,11 +30,15 @@ class MergedKeyword(object):
     """Fake a 'merged' keyword"""
 
     def __init__(self):
+        # pylint: disable-msg=C0103
+        # We duplicate Keyword naming here
         self.keyword = 'merged'
 
 
 class FakeCard(object):
     """Class which fakes being an AbstractCard for the text view."""
+    # pylint: disable-msg=R0902
+    # Need all the attributes to match AbstractCard
 
     # Keywords which obselete each other
     REPLACE_KEYWORD_MAP = {
@@ -43,8 +52,14 @@ class FakeCard(object):
     }
 
     def __init__(self, oAbsCard):
-        self.oAdvanced = oAbsCard
-        self.oBase = self._find_base_vampire()
+        if oAbsCard.level:
+            self.oAdvanced = oAbsCard
+            self.oBase = self._find_base_vampire()
+        else:
+            self.oBase = oAbsCard
+            self.oAdvanced = self._find_adv_vampire()
+        # pylint: disable-msg=C0103
+        # We duplicate AbstractCard naming here
         self.name = self.oAdvanced.name.replace('(Advanced)', '(Merged)')
         # These are never set
         self.cost = None
@@ -56,18 +71,18 @@ class FakeCard(object):
         self.artists = []
         self.virtue = []
         # Take these from the advanced card
-        self.capacity = oAbsCard.capacity
-        self.group = oAbsCard.group
-        self.cardtype = oAbsCard.cardtype
-        self.clan = oAbsCard.clan
-        self.sect = oAbsCard.sect
-        self.discipline = [x for x in oAbsCard.discipline]
+        self.capacity = self.oAdvanced.capacity
+        self.group = self.oAdvanced.group
+        self.cardtype = self.oAdvanced.cardtype
+        self.clan = self.oAdvanced.clan
+        self.sect = self.oAdvanced.sect
+        self.discipline = [x for x in self.oAdvanced.discipline]
         self.keywords = [x for x in self.oBase.keywords]
-        for oKeyword in oAbsCard.keywords:
+        for oKeyword in self.oAdvanced.keywords:
             if oKeyword not in self.keywords:
                 self.keywords.append(oKeyword)
-        if oAbsCard.title:
-            self.title = [x for x in oAbsCard.title]
+        if self.oAdvanced.title:
+            self.title = [x for x in self.oAdvanced.title]
         elif self.oBase.title and self.oBase.sect == self.sect:
             self.title = [x for x in self.oBase.title]
         else:
@@ -78,6 +93,8 @@ class FakeCard(object):
         self._fix_keywords()
 
     def _fix_keywords(self):
+        """Fix keywords to remove duplicates and merge things as
+           approriates."""
         for oReplacement, aObselete in self.REPLACE_KEYWORD_MAP.items():
             if oReplacement in self.keywords:
                 for oKeyword in aObselete:
@@ -88,6 +105,9 @@ class FakeCard(object):
 
     def _fix_special_cases(self):
         """This is a long and tedious set of cases we need to handle."""
+        # pylint: disable-msg=R0915, R0912
+        # We can't really shorten this, due to the number
+        # of individual special cases.
         if self.name == 'Al-Ashrad, Amr of Alamut (Merged)':
             self.title = [ITitle('Inner Circle')]
             self.keywords.append(IKeyword('3 bleed'))
@@ -224,13 +244,34 @@ class FakeCard(object):
         self.text = normalise_whitespace(self.text)
 
     def _find_base_vampire(self):
+        """Find the corresponding base vampire.
+
+           Due to the checks in the plugin, this is assumed to be safe."""
+        # pylint: disable-msg=E1103
+        # pyprotocols confuses pylint
         sBaseName = self.oAdvanced.name.replace(' (Advanced)', '')
         # Special cases
         if '(EC 2013)' in sBaseName:
             sBaseName = sBaseName.replace(' (EC 2013)', '')
         return IAbstractCard(sBaseName)
 
+    def _find_adv_vampire(self):
+        """Find the corresponding advanced vampire
+
+           Due to the checks in the plugin, this is assumed to be safe."""
+        # pylint: disable-msg=E1103
+        # pyprotocols confuses pylint
+        sAdvName = self.oBase.name + ' (Advanced)'
+        # Note that base brunhilde links to the non-storyline advanced version
+        return IAbstractCard(sAdvName)
+
     def _make_merged_text(self):
+        """Combine the text of the two versions, cleaning up keyword
+           phrases and mostly handling the sect / title prefixes.
+           Special cases and hard to automatically fix isses will be
+           cleaned up in the special case code."""
+        # pylint: disable-msg=E1103
+        # pyprotocols confuses pylint
         if ':' in self.oBase.text:
             sBaseText = self.oBase.text.split(':', 1)[1]
         else:
@@ -292,10 +333,12 @@ class MergedVampirePlugin(SutekhPlugin):
             '<span size="x-small">Show Base</span>')
         self._bMerged = False
         self._oMerged.connect('clicked', self._merge_vampire)
-        self._oAdv.connect('clicked', self._reset_vampire)
+        self._oAdv.connect('clicked', self._show_adv_vampire)
         self._oBase.connect('clicked', self._show_base_vampire)
         self._oAbsCard = None
         self._oFakeCard = None
+        self._aBaseVamps = set()
+        self._make_base_map()
 
     def cleanup(self):
         """Remove the listener"""
@@ -319,33 +362,74 @@ class MergedVampirePlugin(SutekhPlugin):
         self._oAbsCard = oPhysCard.abstractCard
         self._oFakeCard = None
         if self._oAbsCard.level is None:
-            return
+            if self._oAbsCard not in self._aBaseVamps:
+                return
+        self._oFakeCard = FakeCard(self._oAbsCard)
         oCardTextView = self.parent.card_text_pane.view
+        # Button logic
+        # For the normal view, we show the buttons
+        # "Merged" and "Other version" (Base / Advanced)
+        # Because of how add_button_to_text is implemented, we
+        # add the second button first
+        if self._oAbsCard.level is None:
+            oCardTextView.add_button_to_text(self._oAdv, "  ")
+        else:
+            oCardTextView.add_button_to_text(self._oBase, "  ")
         oCardTextView.add_button_to_text(self._oMerged, "\n  ")
+
+    def _make_base_map(self):
+        """Find all the vampires with advanced versions."""
+        aAllAdvanced = list(SutekhAbstractCard.selectBy(level='advanced'))
+        for oAbsCard in aAllAdvanced:
+            sBaseName = oAbsCard.name.replace(' (Advanced)', '')
+            try:
+                oBaseCard = IAbstractCard(sBaseName)
+                self._aBaseVamps.add(oBaseCard)
+            except SQLObjectNotFound:
+                # we skip the special cases here
+                continue
 
     def _merge_vampire(self, _oWidget):
+        """Display the merged version of the vampire."""
         oCardTextView = self.parent.card_text_pane.view
         oCardTextView.clear_text()
-        if not self._oFakeCard:
-            self._oFakeCard = FakeCard(self._oAbsCard)
 
         oCardTextView.print_card_to_buffer(self._oFakeCard)
-        oCardTextView.add_button_to_text(self._oBase, "    ")
-        oCardTextView.add_button_to_text(self._oAdv, "\n  ")
+        # We show the buttons as "Normal" and "Other version"
+        # where "Normal" is the card that was selected to intialise
+        # the text view
+        if self._oAbsCard == self._oFakeCard.oAdvanced:
+            oCardTextView.add_button_to_text(self._oBase, "  ")
+            oCardTextView.add_button_to_text(self._oAdv, "\n  ")
+        else:
+            # swap button order
+            oCardTextView.add_button_to_text(self._oAdv, "  ")
+            oCardTextView.add_button_to_text(self._oBase, "\n  ")
 
-    def _reset_vampire(self, _oWidget):
-        """Restore the advanced version"""
+    def _show_adv_vampire(self, _oWidget):
+        """Display the advanced version"""
         oCardTextView = self.parent.card_text_pane.view
         oCardTextView.clear_text()
-        oCardTextView.print_card_to_buffer(self._oAbsCard)
-        oCardTextView.add_button_to_text(self._oMerged, "\n  ")
+        oCardTextView.print_card_to_buffer(self._oFakeCard.oAdvanced)
+        # Duplicates button logic in post_set_card_text
+        if self._oAbsCard == self._oFakeCard.oAdvanced:
+            oCardTextView.add_button_to_text(self._oBase, "  ")
+            oCardTextView.add_button_to_text(self._oMerged, "\n  ")
+        else:
+            oCardTextView.add_button_to_text(self._oMerged, "  ")
+            oCardTextView.add_button_to_text(self._oBase, "\n  ")
 
     def _show_base_vampire(self, _oWidget):
-        """Restore the advanced version"""
+        """Display the base version"""
         oCardTextView = self.parent.card_text_pane.view
         oCardTextView.clear_text()
         oCardTextView.print_card_to_buffer(self._oFakeCard.oBase)
-        oCardTextView.add_button_to_text(self._oMerged, "\n  ")
+        if self._oAbsCard == self._oFakeCard.oBase:
+            oCardTextView.add_button_to_text(self._oAdv, "  ")
+            oCardTextView.add_button_to_text(self._oMerged, "\n  ")
+        else:
+            oCardTextView.add_button_to_text(self._oMerged, "  ")
+            oCardTextView.add_button_to_text(self._oAdv, "\n  ")
 
 
 plugin = MergedVampirePlugin
