d14d0c58aae08b8b1ca08f870e280b3edc5f0af3
Neil Muller <drnlmuller+sutekh@gmail.com>
2017-06-07 08:24:49 +0000

    Include Anthology card in Mixed Card back check
    We check against sets of backs now, rather than just indivual expansions,
    in the hope that more VEKN printed expansions will appear.

--- sutekh/sutekh/gui/plugins/AnalyzeCardList.py
+++ sutekh/sutekh/gui/plugins/AnalyzeCardList.py
@@ -25,7 +25,7 @@ from sutekh.base.gui.GuiUtils import wrap
 from sutekh.base.gui.MultiSelectComboBox import MultiSelectComboBox
 from sutekh.base.gui.AutoScrolledWindow import AutoScrolledWindow
 
-ODD_BACKS = set()
+ODD_BACKS = {}
 
 PDF_SETS = set()
 
@@ -74,11 +74,13 @@ def _load_odd_backs():
     """Cache info about the non-standard card backs."""
     ODD_BACKS.clear()
     try:
-        oThirdEd = IExpansion('Third Edition')
-        oJyhad = IExpansion('Jyhad')
-        ODD_BACKS.add(oThirdEd)
-        ODD_BACKS.add(oJyhad)
-        ODD_BACKS.add(None)
+        aThirdEd = set([IExpansion('Third Edition')])
+        aJyhad = set([IExpansion('Jyhad')])
+        aVEKNPrinted = set([IExpansion('Anthology')])
+        ODD_BACKS['Third Edition'] = aThirdEd
+        ODD_BACKS['Jyhad'] = aJyhad
+        ODD_BACKS['VEKN Printed Set'] = aVEKNPrinted
+        ODD_BACKS['Unknown'] = set([None])
     except SQLObjectNotFound as oExcDetails:
         # log exception at same level as plugin errors (verbose)
         logging.warn("Expansion caching failed (%s).", oExcDetails, exc_info=1)
@@ -265,8 +267,16 @@ def _get_back_counts(aPhysCards):
         oKey = oCard.expansion
         if oKey in PDF_SETS:
             oKey = 'PDF'
-        elif oKey not in ODD_BACKS:
-            oKey = 'Other'
+        else:
+            bOther = True
+            for sName, oSet in ODD_BACKS.items():
+                if oKey in oSet:
+                    oKey = sName
+                    bOther = False
+                    break
+            if bOther:
+                # Fell through without a match
+                oKey = 'Other'
         dCounts.setdefault(oKey, 0)
         dCounts[oKey] += 1
     return dCounts
@@ -327,13 +337,10 @@ def _percentage_backs(dCards, iSize, fPer, sType):
        small"""
     sText = ""
     bOK = True
-    for oExp in dCards:
-        fThisPer = float(dCards[oExp]) / float(iSize) * 100
-        if hasattr(oExp, 'name'):
-            sName = oExp.name
-        else:
-            sName = oExp
-        sText += "%d %s backs (%2.1f%% of the %s).\n" % (dCards[oExp], sName,
+    for sGroupName in dCards:
+        fThisPer = float(dCards[sGroupName]) / float(iSize) * 100
+        sText += "%d %s backs (%2.1f%% of the %s).\n" % (dCards[sGroupName],
+                                                         sGroupName,
                                                          fThisPer,
                                                          sType.lower())
         if fThisPer < fPer:
@@ -350,13 +357,16 @@ def _group_backs(dCards, aCards, iNum):
     dCardsByExp = {}
     bOK = True
     sText = ""
-    for oExp in dCards:
-        if oExp != 'Other':
-            dCardsByExp[oExp] = [oCard for oCard in aCards if
-                                 oCard.expansion == oExp]
+    aAllOdd = set()
+    for aExps in ODD_BACKS.values():
+        aAllOdd.update(aExps)
+    for sGrpName in dCards:
+        if sGrpName != 'Other':
+            dCardsByExp[sGrpName] = [oCard for oCard in aCards if
+                                     oCard.expansion in ODD_BACKS[sGrpName]]
         else:
-            dCardsByExp[oExp] = [oCard for oCard in aCards if
-                                 oCard.expansion not in ODD_BACKS]
+            dCardsByExp[sGrpName] = [oCard for oCard in aCards if
+                                     oCard.expansion not in aAllOdd]
     for oExp, aExpCards in dCardsByExp.iteritems():
         # For each expansion, count number of distinct cards
         aNames = set([x.abstractCard.name for x in aExpCards])
