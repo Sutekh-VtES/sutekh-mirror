5380bf0b0b5fa6361c529c19a2f9ee8d79c0c3d1
Neil Muller <drnlmuller+sutekh@gmail.com>
2017-07-11 19:57:10 +0000

    Point CardImages at temporary download site

--- sutekh/sutekh/gui/plugins/CardImages.py
+++ sutekh/sutekh/gui/plugins/CardImages.py
@@ -21,9 +21,8 @@ from sutekh.base.gui.plugins.BaseImages import (BaseImageFrame,
                                                 CARD_IMAGE_PATH,
                                                 DOWNLOAD_IMAGES)
 
-
-# We try lackeyccg for images from these sets
-LACKEY_IMAGES = ('dm', 'vekn_2014_the_returned', 'tu', 'promo', 'au')
+# Base url for downloading the images from
+SUTEKH_IMAGE_SITE = 'https://sutekh.drnlm.org/'
 
 
 class CardImageFrame(BaseImageFrame):
@@ -78,52 +77,14 @@ class CardImageFrame(BaseImageFrame):
         return sExpName
 
     def _make_card_urls(self, _sFullFilename):
-        """Return a url pointing to the vtes.pl scan of the image"""
+        """Return a url pointing to the scan of the image"""
         sCurExpansionPath = self._convert_expansion(self._sCurExpansion)
         sFilename = self._norm_cardname(self._sCardName)[0]
         if sCurExpansionPath == '' or sFilename == '':
             # Error path, we don't know where to search for the image
             return None
-        # Remap paths to point to the vtes.pl equivilents
-        if sCurExpansionPath == 'nergal_storyline':
-            sCurExpansionPath = 'isl'
-        elif sCurExpansionPath == 'anarchs_and_alastors_storyline':
-            sCurExpansionPath = 'aa'
-        elif sCurExpansionPath == 'edens_legacy_storyline':
-            sCurExpansionPath = 'el'
-        elif sCurExpansionPath == 'cultist_storyline':
-            sCurExpansionPath = 'csl'
-        elif sCurExpansionPath == 'white_wolf_2003_demo':
-            sCurExpansionPath = 'dd'
-        elif sCurExpansionPath == 'third':
-            sCurExpansionPath = '3e'
-        if sCurExpansionPath == 'tenth' and sFilename == 'carltonvanwyk.jpg':
-            # point to pre-Antology card name
-            sFilename = 'carltonvanwykhunter.jpg'
-        sUrl = 'http://nekhomanta.h2.pl/pics/games/vtes/%s/%s' % (
-            sCurExpansionPath, sFilename)
-        if sCurExpansionPath in LACKEY_IMAGES:
-            # Try get the card from lackey first
-            # Strip the "(Mythic storyline)" prefix from the returned cards
-            sFilename2 = None
-            if sCurExpansionPath == 'vekn_2014_the_returned':
-                # Lackey uses 'montaro2' and so forth for the duplicate names
-                # between the 2015 storyline rewards expansion and the actual
-                # storyline cards, but this only applies to some of the card
-                # names, so we need to try all possibilities in the right
-                # order
-                sFilename = sFilename.replace('mythicstoryline', '')
-                sFilename2 = sFilename.replace('.jpg', '2.jpg')
-            sLackeyUrl = 'http://www.lackeyccg.com/vtes/high/cards/%s' % (
-                sFilename)
-            if sFilename2:
-                sLackeyUrl2 = 'http://www.lackeyccg.com/vtes/high/cards/%s' % (
-                    sFilename2)
-                return (sLackeyUrl2, sLackeyUrl, sUrl)
-            if sCurExpansionPath == 'promo':
-                # We prefer nekhomanta.h2.pl for older promos
-                return (sUrl, sLackeyUrl)
-            return (sLackeyUrl, sUrl)
+        sUrl = '%s/cardimages/%s/%s' % (SUTEKH_IMAGE_SITE, sCurExpansionPath,
+                                        sFilename)
         return (sUrl, )
 
     def _norm_cardname(self, sCardName):
@@ -146,9 +107,11 @@ class ImageConfigDialog(BaseImageConfigDialog):
     # R0904 - gtk Widget, so has many public methods
     """Dialog for configuring the Image plugin."""
 
-    sDefURLId = 'csillagbolcselet.hu'
-    sDefaultUrl = 'http://csillagmag.hu/upload/pictures.zip'
-    sImgDownloadSite = 'vtes.pl'
+    # These two are descriptive, so set them to the final value
+    sDefUrlId = 'sutekh.vtes.za.net'
+    sImgDownloadSite = 'sutekh.vtes.za.net'
+    # Will be changed later
+    sDefaultUrl = '%s/zipped/%s' % (SUTEKH_IMAGE_SITE, 'cardimages.zip')
 
     def __init__(self, oImagePlugin, bFirstTime=False, bDownloadUpgrade=False):
         super(ImageConfigDialog, self).__init__(oImagePlugin, bFirstTime)
@@ -166,9 +129,9 @@ class ImageConfigDialog(BaseImageConfigDialog):
                                        'cardimages plugin</b>\n'
                                        'The card images plugin can now '
                                        'download missing images from '
-                                       'vtes.pl.\nDo you wish to enable '
+                                       '%s.\nDo you wish to enable '
                                        'this (you will not be prompted '
-                                       'again)?')
+                                       'again)?' % SUTEKH_IMAGE_SITE)
             self.vbox.pack_start(self.oDescLabel, False, False)
             self.vbox.pack_start(self.oDownload, False, False)
             self.set_size_request(400, 200)
