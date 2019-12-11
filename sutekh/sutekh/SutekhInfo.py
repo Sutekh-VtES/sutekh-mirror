926147ada9a905e065a9b11c5047afc1972a9f7c
Neil Muller <drnlmuller+sutekh@gmail.com>
2019-12-11 21:38:48 +0000

    Bump version on trunk for future breakage

--- sutekh/sutekh/SutekhInfo.py
+++ sutekh/sutekh/SutekhInfo.py
@@ -19,7 +19,7 @@ PYTHON = 'License :: OSI Approved :: Python Software Foundation License',
 
 # pylint: disable=too-few-public-methods, missing-docstring
 class SutekhInfo(object):
-    VERSION = (1, 0, 0, 'rc', 1)
+    VERSION = (1, 1, 0, 'alpha', 1)
     BASE_VERSION_STR = '.'.join([str(x) for x in VERSION[:3]])
     VERSION_STR = {
         'final': BASE_VERSION_STR,
