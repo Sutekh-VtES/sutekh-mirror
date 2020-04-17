193cab62bda42ee13a2c2f0ba396d1163010dfe6
Neil Muller <drnlmuller+sutekh@gmail.com>
2020-04-17 07:30:01 +0000

    Strip doubled /'s so we don't get 308 redirects which break urllib

--- sutekh/io/WwUrls.py
+++ sutekh/io/WwUrls.py
@@ -8,17 +8,17 @@
 
 WW_CARDLIST_DATAPACK = "cardlist"
 
-WW_CARDLIST_URL = ("https://gitlab.com/sutekh//sutekh-extras/"
+WW_CARDLIST_URL = ("https://gitlab.com/sutekh/sutekh-extras/"
                    "-/raw/master/CardList/cardlist-data/cardlist.txt")
 
-EXTRA_CARD_URL = ("https://gitlab.com/sutekh//sutekh-extras/"
+EXTRA_CARD_URL = ("https://gitlab.com/sutekh/sutekh-extras/"
                   "-/raw/master/extra_list.txt")
 
-WW_RULINGS_URL = ("https://gitlab.com/sutekh//sutekh-extras/"
+WW_RULINGS_URL = ("https://gitlab.com/sutekh/sutekh-extras/"
                   "-/raw/master/Rulebooks/rulebook-data/rulings.html")
 
-EXP_DATA_URL = ("https://gitlab.com/sutekh//sutekh-extras/"
+EXP_DATA_URL = ("https://gitlab.com/sutekh/sutekh-extras/"
                 "-/raw/master/ExpansionDates/expansion-data/expansions.json")
 
-LOOKUP_DATA_URL = ("https://gitlab.com/sutekh//sutekh-extras/"
+LOOKUP_DATA_URL = ("https://gitlab.com/sutekh/sutekh-extras/"
                    "-/raw/master/Lookups/lookup.csv")
