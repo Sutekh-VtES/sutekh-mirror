7c58a809cbc87103c168e8c6e70db63719753f8c
Neil Muller <drnlmuller+sutekh@gmail.com>
2020-08-02 10:03:33 +0000

    Avoid hardcoding path to the project

--- ci/appveyor/run_tests.sh
+++ ci/appveyor/run_tests.sh
@@ -5,10 +5,13 @@ set -e
 # We need to use -l when calling the script to get all the correct msys environment
 # variables set, but that also changes the directory away from appveyors checkout
 # location, and the mapping of windows paths into msys means we can't us %CD%
-# to get the correct path without further munging, so we hardcode the path for now
-#
-# Is this entirely static? Don't know, but seems to work.
-cd /c/projects/sutekh-mirror/sutekh
+# to get the correct path without further munging, so we derive the correct directory
+# from the script name
+
+dir=`dirname $0`
+base=`readlink -e $dir`
+cd $base
+cd ../..
 
 # Finally we can try run the tests - same explicit path requirements apply as
 # for pip
