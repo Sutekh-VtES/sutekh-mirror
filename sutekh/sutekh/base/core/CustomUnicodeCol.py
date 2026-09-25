cc4118eb77fbbab3a4a651c26fffdd7cc7b23bc7
Neil Muller <drnlmuller+sutekh@gmail.com>
2026-09-25 18:54:59 +0200

    Update comment

--- sutekh/sutekh/base/core/CustomUnicodeCol.py
+++ sutekh/sutekh/base/core/CustomUnicodeCol.py
@@ -1,7 +1,13 @@
-# Custom column that uses LONGTEXT for mysql databases
+# Custom column that uses MEDIUMTEXT for mysql databases
 # This matters because the mysql TEXT column is very small,
 # and VARCHAR won't fix it because it's limited by the table
 # size limt, which is also small
+#
+# We choose MEDIUMTEXT over LONGTEXT because it's
+# a) almost certainly good enough
+# b) doesn't require fiddling with max_allowed_packet_size to
+#    allow remote connections to work correctly with larger
+#    entries
 
 
 from sqlobject.col import SOUnicodeCol, Col
